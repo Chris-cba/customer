5a6e10db413e3a985902cb34aaf99265382b0a09
mhuitson <mhuitson@37afec09-bd54-7940-9fac-cf987e4a02e6>
2008-12-12 18:50:56 +0000

    Minor modifications following testing.
    git-svn-id: svn://127.0.0.1/customer/trunk@208 37afec09-bd54-7940-9fac-cf987e4a02e6

--- hillingdon/mai_webservice/install/Exor_mai_ws_20_inst.sql
+++ hillingdon/mai_webservice/install/Exor_mai_ws_20_inst.sql
@@ -1,17 +1,22 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/customer/hillingdon/mai_webservice/install/Exor_mai_ws_20_inst.sql-arc   1.0   Dec 12 2008 14:12:28   mhuitson  $
+--       PVCS id          : $Header:   //vm_latest/archives/customer/hillingdon/mai_webservice/install/Exor_mai_ws_20_inst.sql-arc   1.1   Dec 12 2008 18:50:56   mhuitson  $
 --       Module Name      : $Workfile:   Exor_mai_ws_20_inst.sql  $
---       Date into PVCS   : $Date:   Dec 12 2008 14:12:28  $
---       Date fetched Out : $Modtime:   Dec 12 2008 14:12:04  $
---       Version          : $Revision:   1.0  $
+--       Date into PVCS   : $Date:   Dec 12 2008 18:50:56  $
+--       Date fetched Out : $Modtime:   Dec 12 2008 18:36:20  $
+--       Version          : $Revision:   1.1  $
 --       Based on SCCS version : 
 -------------------------------------------------------------------------
 /*
+||Send output to a log file.
+*/
+spool Exor_mai_ws_20_inst.log;
+
+/*
 ||Create Table defect_list_temp.
 */
-CREATE OR REPLACE GLOBAL TEMPORARY TABLE defect_list_temp
+CREATE GLOBAL TEMPORARY TABLE defect_list_temp
   (dlt_defect_id NUMBER(8))
   ON COMMIT DELETE ROWS
   NOCACHE
@@ -23,3 +28,23 @@ CREATE OR REPLACE GLOBAL TEMPORARY TABLE defect_list_temp
 ALTER TABLE defect_list_temp
   ADD (CONSTRAINT dlt_pk PRIMARY KEY (dlt_defect_id))
 /
+
+@register_exor_mai_ws_schemas
+
+@mai_api.pkh
+
+@mai_api.pkw
+
+@mai_web_service.pkh
+
+@mai_web_service.pkw
+
+spool off;
+
+@compile_schema
+
+spool compile_all.log;
+
+@compile_all
+
+spool off;
