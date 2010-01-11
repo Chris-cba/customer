e50c7f6ad85032cf6babf7b34a7f8bd44ea61a3e
swilliams <swilliams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2010-01-11 12:11:04 +0000

    Amended Ds to Ls as Sineco have gone from DoT to Local referencing.  Added PVCS headers.
    git-svn-id: svn://127.0.0.1/customer/trunk@351 37afec09-bd54-7940-9fac-cf987e4a02e6

--- sineco/admin/sql/sineco_xsp_restraints.sql
+++ sineco/admin/sql/sineco_xsp_restraints.sql
@@ -12,13 +12,13 @@ CREATE OR REPLACE FORCE VIEW xsp_restraints (xsr_nw_type,
 AS
    SELECT 
 --
---   SCCS Identifiers :-
+--   PVCS Identifiers :-
 --
---       sccsid           : @(#)xsp_restraints.vw  1.3 01/25/07
---       Module Name      : xsp_restraints.vw
---       Date into SCCS   : 07/01/25 15:48:11
---       Date fetched Out : 07/06/13 17:08:43
---       SCCS Version     : 1.3
+--       pvcsid           : $Header:   //vm_latest/archives/customer/sineco/admin/sql/sineco_xsp_restraints.sql-arc   3.1   Jan 11 2010 12:11:04   swilliams  $
+--       Module Name      : $Workfile:   sineco_xsp_restraints.sql  $
+--       Date into PVCS   : $Date:   Jan 11 2010 12:11:04  $
+--       Date fetched Out : $Modtime:   Jan 11 2010 12:10:20  $
+--       PVCS Version     : $Revision:   3.1  $
 --
 -----------------------------------------------------------------------------
 -- Copyright (c) exor corporation ltd, 2004
@@ -29,13 +29,14 @@ AS
 -- 20-NOV-2009 the silly looking decode for network type is to force
 -- the view column to varchar2(4) otherwise nm3inv package gives 
 -- signature errors.
---
-          decode(xsr_nw_type,xsr_nw_type,'D',null,'D','D   ') xsr_nw_type, xsr_ity_inv_code, xsr_scl_class, xsr_x_sect_value,
+-- 11-JAN-2010 SW amended Ds to Ls as Sineco have gone from DoT to Local
+--             referencing.
+          decode(xsr_nw_type,xsr_nw_type,'L',null,'L','L   ') xsr_nw_type, xsr_ity_inv_code, xsr_scl_class, xsr_x_sect_value,
           xsr_descr, xsr_date_created, xsr_date_modified, xsr_modified_by,
           xsr_created_by
      FROM nm_xsp_restraints
    UNION
-   SELECT 'D' nwx_nw_type, '$$', nwx_nsc_sub_class, nwx_x_sect, nwx_descr,
+   SELECT 'L' nwx_nw_type, '$$', nwx_nsc_sub_class, nwx_x_sect, nwx_descr,
           NULL, NULL, NULL, NULL
      FROM nm_xsp;
 
