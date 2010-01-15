3a08f98067dac042a4bc14b3169720f4348451d2
malexander <malexander@37afec09-bd54-7940-9fac-cf987e4a02e6>
2010-01-15 11:10:46 +0000

    Changes to allow install/upgrade/fix determined by script and appropriate actions taken.
    git-svn-id: svn://127.0.0.1/customer/trunk@353 37afec09-bd54-7940-9fac-cf987e4a02e6

--- WAG/pla/install/pla_install.sql
+++ WAG/pla/install/pla_install.sql
@@ -1,18 +1,20 @@
 -------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       PVCS id          : $Header:   //vm_latest/archives/customer/WAG/pla/install/pla_install.sql-arc   3.5   Jan 14 2010 17:22:50   malexander  $
+--       PVCS id          : $Header:   //vm_latest/archives/customer/WAG/pla/install/pla_install.sql-arc   3.6   Jan 15 2010 11:10:46   malexander  $
 --       Module Name      : $Workfile:   pla_install.sql  $
---       Date into PVCS   : $Date:   Jan 14 2010 17:22:50  $
---       Date fetched Out : $Modtime:   Jan 14 2010 17:22:36  $
---       Version          : $Revision:   3.5  $
+--       Date into PVCS   : $Date:   Jan 15 2010 11:10:46  $
+--       Date fetched Out : $Modtime:   Jan 15 2010 11:09:30  $
+--       Version          : $Revision:   3.6  $
 -------------------------------------------------------------------------
+--	Copyright (c) Exor Corporation Ltd, 2010
+-------------------------------------------------------------------------
+--
 --
 set echo off
 set linesize 120
 set heading off
 set feedback off
---
 --
 ---------------------------------------------------------------------------------------------------
 --                             ****************** LOG FILE *******************
@@ -23,50 +25,78 @@ col         log_extension new_value log_extension noprint
 set term off
 select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
 /
+
+---
+-- The following pl/sql variables must be defined (some will be null) for the appropriately named logfile to be generated
+--
+--
+--   VARIABLE     |  RULES FOR SETTING                                                 | EXAMPLE VALUE(S)
+-- ===============================================================================================================
+--   script_mode  |  Mandatory                                                         | 'INSTALL' 'UPGRADE' 'FIX'
+--   from_version |  Mandatory when script_mode is 'UPGRADE' or 'FIX', null otherwise  | 4052
+--   to_version   |  Mandatory when script_mode is 'UPGRADE', null otherwise           | 4100
+--   fix_number   |  Mandatory when script_mode is 'FIX', null otherwise               | 1
+--
+--
+
+undefine proper_to_version
+col      proper_to_version new_value proper_to_version noprint
+select substr(&to_version,1,1)||'.'||substr(&to_version,2,1)||'.'||substr(&to_version,3,1)||'.'||substr(&to_version,4,1) proper_to_version
+from dual
+/  
+
+undefine logfile1
+col      logfile1 new_value logfile1 noprint
+undefine logfile2
+col      logfile2 new_value logfile2 noprint   
+
+set term off
+select  case when UPPER('&script_mode') = 'INSTALL' THEN 
+                  'pla_install_&log_extension' 
+             when UPPER('&script_mode') = 'UPGRADE' THEN 
+                  'pla'||&from_version||'_pla'||&to_version||'_'||'&log_extension'
+             else
+                  'pla'||&from_version||'_fix'||&fix_number||'_'||'&log_extension' 
+             end logfile                        
+from dual             
+/
 set term on
 
-define logfile1='pla_install_1_&log_extension'
-define logfile2='pla_install_2_&log_extension'
 spool &logfile1
 --
 ---------------------------------------------------------------------------------------------------
 --                                     ********** CHECKS  ***********
-select 'Installation Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MM:SS') from dual;
+select 'Date ' || to_char(sysdate, 'DD-MON-YYYY HH24:MM:SS') from dual;
 
-SELECT 'Install Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
+SELECT 'Running on ' ||LOWER(USER||'@'||instance_name||'.'||host_name)||' - DB ver : '||version
 FROM v$instance;
 
 SELECT 'Current version of '||hpr_product||' ' ||hpr_version
 FROM hig_products
-WHERE hpr_product IN ('HIG','NET');
+WHERE hpr_product IN ('HIG','NET',null);
 
 WHENEVER SQLERROR EXIT
+
 --
--- Check that the user isn't sys or system
+-- Check that the user isn't sys or system AND that the correct version of APEX is installed.
 --
+DECLARE
+  l_count number; 
 BEGIN
    --
       IF USER IN ('SYS','SYSTEM')
        THEN
          RAISE_APPLICATION_ERROR(-20000,'You cannot install this product as ' || USER);
       END IF;
+      
+      select count(username) into l_count from dba_users where username like '%0302%';
+      IF l_count < 1
+       THEN
+         RAISE_APPLICATION_ERROR(-20001,'You do not have the correct version of Apex installed (3.0.2.0) '); 
+      END IF;
 END;
 /
 
--- Check that the PLA has not already been installed
-BEGIN
-   --
-   FOR v_recs IN (SELECT hpr_version
-                  FROM user_tables
-                      ,hig_products
-                  WHERE hpr_product = 'PLA'
-                  AND   table_name = 'WAG_PLAN_RECORD_CARD') LOOP
-
-               RAISE_APPLICATION_ERROR(-20000,'PLA version '||v_recs.hpr_version||' already installed.');
-
-   END LOOP;
-END;
-/
 
 --
 -- Check that PEM has been installed @ v4.1.0.0 as PLA is dependent on this
@@ -86,9 +116,23 @@ SET TERM ON
 Prompt Tables...
 SET TERM OFF
 SET DEFINE ON
-select '&exor_base'||'pla'||'&terminator'||'install'||
-       '&terminator'||'pla.tab' run_file
-from dual
+Select '&exor_base'||'pla'||'&terminator'||'install'||
+        '&terminator'||'pla.tab' run_file
+From   dual
+Where Not Exists (Select 'Install'
+                  From   hig_products
+                  Where  hpr_product = 'PLA'
+                  And    hpr_version In ('4.0.5.2','4.1.0.0')
+                 )
+Union
+Select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy.sql'
+From   hig_products
+Where Exists (Select 'Upgrade From 4052'
+              From   hig_products
+              Where  hpr_product = 'PLA'
+              And    hpr_version = '4.0.5.2'
+             )
 /
 SET FEEDBACK ON
 start &&run_file
@@ -102,7 +146,21 @@ SET TERM OFF
 SET DEFINE ON
 select '&exor_base'||'pla'||'&terminator'||'install'||
        '&terminator'||'pla.ind' run_file
-from dual
+From   dual
+Where Not Exists (Select 'Install'
+                  From   hig_products
+                  Where  hpr_product = 'PLA'
+                  And    hpr_version In ('4.0.5.2','4.1.0.0')
+                 )
+Union
+Select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy.sql'
+From   hig_products
+Where Exists (Select 'Upgrade From 4052'
+              From   hig_products
+              Where  hpr_product = 'PLA'
+              And    hpr_version = '4.0.5.2'
+             )
 /
 SET FEEDBACK ON
 start &&run_file
@@ -116,7 +174,21 @@ SET TERM OFF
 SET DEFINE ON
 select '&exor_base'||'pla'||'&terminator'||'install'||
        '&terminator'||'pla.con' run_file
-from dual
+From   dual
+Where Not Exists (Select 'Install'
+                  From   hig_products
+                  Where  hpr_product = 'PLA'
+                  And    hpr_version In ('4.0.5.2','4.1.0.0')
+                 )
+Union
+Select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy.sql'
+From   hig_products
+Where Exists (Select 'Upgrade From 4052'
+              From   hig_products
+              Where  hpr_product = 'PLA'
+              And    hpr_version = '4.0.5.2'
+             )
 /
 SET FEEDBACK ON
 start &&run_file
@@ -130,7 +202,21 @@ SET TERM OFF
 SET DEFINE ON
 select '&exor_base'||'pla'||'&terminator'||'install'||
        '&terminator'||'pla.sqs' run_file
-from dual
+From   dual
+Where Not Exists (Select 'Install'
+                  From   hig_products
+                  Where  hpr_product = 'PLA'
+                  And    hpr_version In ('4.0.5.2','4.1.0.0')
+                 )
+Union
+Select '&exor_base'||'nm3'||'&terminator'||'install'||
+        '&terminator'||'dummy.sql'
+From   hig_products
+Where Exists (Select 'Upgrade From 4052'
+              From   hig_products
+              Where  hpr_product = 'PLA'
+              And    hpr_version = '4.0.5.2'
+             )
 /
 SET FEEDBACK ON
 start &&run_file
@@ -315,14 +401,50 @@ SET FEEDBACK OFF
 SET TERM ON
 Prompt Setting The Version Number...
 SET TERM OFF
-BEGIN
-      hig2.upgrade('PLA','pla_install.sql','Installed','4.1.0.0');
-END;
+
+undefine hig_upgrades_sql
+col      hig_upgrades_sql new_value hig_upgrades_sql noprint
+set term off
+select  case when UPPER('&script_mode') = 'INSTALL' THEN 
+                  'begin hig2.upgrade(''''PLA'''',''''pla_install.sql'''',''''Installed'''',''''&proper_to_version''''); end;'
+             when UPPER('&script_mode') = 'UPGRADE' THEN 
+                  'begin hig2.upgrade(''''PLA'''',''''pla_install.sql'''',''''Upgrade from &from_version to &to_version'''',''''&proper_to_version''''); end;'
+             else
+                  'begin hig2.upgrade(''''PLA'''',''''pla_install.sql'''',''''PLA Fix &fix_number'''',Null); end;'
+             end hig_upgrades_sql                        
+from dual             
+/
+
+begin
+execute immediate '&hig_upgrades_sql';
+end;
 /
 COMMIT;
-SELECT 'Product installed at version '||hpr_product||' ' ||hpr_version details
+
+
+select  case when UPPER('&script_mode') = 'INSTALL' THEN 
+                  'Product installed at version '||hpr_product||' ' ||hpr_version 
+             when UPPER('&script_mode') = 'UPGRADE' THEN 
+                  'Product upgraded to version '||hpr_product||' ' ||hpr_version 
+             else
+                  'Fix applied'
+             end details
 FROM hig_products
 WHERE hpr_product IN ('PLA');
+--
+---------------------------------------------------------------------------------------------------
+--                    ********************* APPLICATION AND PODS *************************
+SET TERM ON
+Prompt Application and Pods...
+SET TERM OFF
+SET DEFINE ON  
+select '&exor_base'||'pla'||'&terminator'||'admin'||
+        '&terminator'||'sql'||'&terminator'||'f624_wag_planning_v24' run_file
+from dual
+/
+SET FEEDBACK ON
+start '&&run_file'
+SET FEEDBACK OFF
 --
 --
 spool off
