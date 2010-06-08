6d4d5cd5d0378b36426844699a62f1eb924b35e4
aedwards <aedwards@37afec09-bd54-7940-9fac-cf987e4a02e6>
2010-06-08 09:30:46 +0000

    4210 compatability
    git-svn-id: svn://127.0.0.1/customer/trunk@401 37afec09-bd54-7940-9fac-cf987e4a02e6

--- tfl/Task 0108913 - FTP Solution/0108913_install.sql
+++ tfl/Task 0108913 - FTP Solution/0108913_install.sql
@@ -5,11 +5,11 @@ SPOOL 0108913_install.log
 --------------------------------------------------------------------------------
 --   PVCS Identifiers :-
 --
---       sccsid           : $Header:   //vm_latest/archives/customer/tfl/Task 0108913 - FTP Solution/0108913_install.sql-arc   3.0   Dec 15 2009 10:06:46   aedwards  $
+--       sccsid           : $Header:   //vm_latest/archives/customer/tfl/Task 0108913 - FTP Solution/0108913_install.sql-arc   3.1   Jun 08 2010 10:30:46   aedwards  $
 --       Module Name      : $Workfile:   0108913_install.sql  $
---       Date into PVCS   : $Date:   Dec 15 2009 10:06:46  $
---       Date fetched Out : $Modtime:   Dec 15 2009 10:03:00  $
---       PVCS Version     : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Jun 08 2010 10:30:46  $
+--       Date fetched Out : $Modtime:   Jun 08 2010 10:28:50  $
+--       PVCS Version     : $Revision:   3.1  $
 --
 --------------------------------------------------------------------------------
 --
@@ -35,7 +35,7 @@ ACCEPT p_pass        CHAR PROMPT 'ENTER THE FTP PASSWORD                     : '
 ACCEPT p_port        CHAR PROMPT 'ENTER THE FTP PORT NUMBER (USUALLY 21)     : ';
 
 PROMPT
-PROMPT Creating Metadata in X_TFL_FTP_DIRS
+PROMPT Creating Metadata in HIG_FTP_CONNECTIONS
 PROMPT
 --
 SET verify OFF;
@@ -49,16 +49,34 @@ DECLARE
   p_user       VARCHAR2(100) := '&P_USER';
   p_pass       VARCHAR2(100) := '&P_PASS';
   p_port       VARCHAR2(100) := '&P_PORT';
+  l_hft_id     NUMBER        := nm3ddl.sequence_nextval('hft_id_seq');
+  l_hfc_id     NUMBER        := nm3ddl.sequence_nextval('hfc_id_seq');
 --
 BEGIN
 --
-  INSERT INTO x_tfl_ftp_dirs
-  ( ftp_type, ftp_contractor, ftp_username, ftp_password, ftp_host
-  , ftp_arc_username, ftp_arc_password, ftp_arc_host, ftp_in_dir
-  , ftp_out_dir, ftp_arc_in_dir, ftp_arc_out_dir, ftp_port, ftp_arc_port)
-  VALUES 
-   ('TMAI',NULL,p_user,p_pass,p_host_ip,NULL, NULL, NULL,p_ftp_folder
-   ,NULL, NULL, NULL, p_port, p_port);
+--  INSERT INTO x_tfl_ftp_dirs
+--  ( ftp_type, ftp_contractor, ftp_username, ftp_password, ftp_host
+--  , ftp_arc_username, ftp_arc_password, ftp_arc_host, ftp_in_dir
+--  , ftp_out_dir, ftp_arc_in_dir, ftp_arc_out_dir, ftp_port, ftp_arc_port)
+--  VALUES 
+--   ('TMAI',NULL,p_user,p_pass,p_host_ip,NULL, NULL, NULL,p_ftp_folder
+--   ,NULL, NULL, NULL, p_port, p_port);
+--
+  INSERT INTO hig_ftp_types
+    ( hft_id, hft_type, hft_descr)
+  SELECT 
+    l_hft_id, 'TMAI','TMA Inspection Files'
+   FROM DUAL;
+--
+  INSERT INTO hig_ftp_connections
+   ( hfc_id, hfc_hft_id, hfc_name, hfc_nau_admin_unit, hfc_nau_unit_code
+   , hfc_nau_admin_type, hfc_ftp_username, hfc_ftp_password, hfc_ftp_host
+   , hfc_ftp_port, hfc_ftp_in_dir, hfc_ftp_out_dir)
+  SELECT
+     l_hfc_id, l_hft_id, 'TMA_FTP',NULL, NULL
+   , NULL, p_user, nm3ftp.obfuscate_password( p_pass ), p_host_ip
+   , p_port, p_ftp_folder, NULL 
+    FROM DUAL;
 --
 END;
 /
