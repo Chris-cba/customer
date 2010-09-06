d171faadb9de4f0fb5719e6acf2b380ded24888b
Ian.Turnbull <Ian.Turnbull@37afec09-bd54-7940-9fac-cf987e4a02e6>
2010-09-06 10:15:32 +0000

    initial revision
    git-svn-id: svn://127.0.0.1/customer/trunk@433 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/FI Reporting/admin/sql/remove_fi_reporting.sql
@@ -0,0 +1,25 @@
+--
+-----------------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       pvcsid                 : $Header:   //vm_latest/archives/customer/Oregon/FI Reporting/admin/Project1/remove_fi_reporting.sql-arc   3.0   Sep 06 2010 11:15:32   Ian.Turnbull  $
+--       Module Name      : $Workfile:   remove_fi_reporting.sql  $
+--       Date into PVCS   : $Date:   Sep 06 2010 11:15:32  $
+--       Date fetched Out : $Modtime:   Aug 31 2010 16:48:24  $
+--       PVCS Version     : $Revision:   3.0  $
+--
+--
+--   Author : P Stanton
+--
+-----------------------------------------------------------------------------
+--	Copyright (c) exor corporation ltd, 2010
+-----------------------------------------------------------------------------
+--
+-- Sctipt to remove Bike Ped
+drop table XODOT_HBUD_FI_REPORT;
+
+drop package XODOT_FI_REPORT;
+
+
+
