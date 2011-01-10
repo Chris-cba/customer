d0968d01ef88be0e30c71c221c05054c97803372
Ian.Turnbull <Ian.Turnbull@37afec09-bd54-7940-9fac-cf987e4a02e6>
2011-01-10 10:36:00 +0000

    initial revision
    git-svn-id: svn://127.0.0.1/customer/trunk@572 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ WAG/pla/admin/sql/welsh_areas_meta.sql
@@ -0,0 +1,409 @@
+--
+-----------------------------------------------------------------------------
+--   PVCS Identifiers :-
+--
+--       pvcsid                     : $Header::
+--       Module Name                : $Workfile::
+--       Date into PVCS             : $Date::
+--       Date fetched Out           : $Modtime::
+--       PVCS Version               : $Revision::
+--       Based on SCCS version      :
+--
+--
+--   Author : H.Buckley
+--
+--   wag_pem_change_resp_trg
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) exor corporation ltd, 2006
+-----------------------------------------------------------------------------
+--
+--
+
+
+insert into hig_domains
+( hdo_domain
+, hdo_product
+, hdo_title
+, hdo_code_length )
+values
+( 'PLANAPPS_REGIONS'
+, 'ENQ'
+, 'Regions of Wales'
+, 2 );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '45'
+, 'ANGLESEY'
+, 'N'
+, 1
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '31'
+, 'BLAENAU GWENT'
+, 'N'
+, 2
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '53'
+, 'BRIDGEND'
+, 'N'
+, 3
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '57'
+, 'CAERPHILLY'
+, 'N'
+, 4
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '21'
+, 'CARMARTHENSHIRE'
+, 'N'
+, 5
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '71'
+, 'CARDIFF'
+, 'N'
+, 6
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '22'
+, 'CEREDIGION'
+, 'N'
+, 7
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '46'
+, 'CONWY'
+, 'N'
+, 8
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '15'
+, 'DENBIGHSHIRE'
+, 'N'
+, 9
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '17'
+, 'FLINTSHIRE'
+, 'N'
+, 10
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '44'
+, 'GWYNEDD'
+, 'N'
+, 11
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '52'
+, 'MERTHYR TYDFIL'
+, 'N'
+, 12
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '33'
+, 'MONMOUTHSHIRE'
+, 'N'
+, 13
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '85'
+, 'NEATH PORT TALBOT'
+, 'N'
+, 14
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '43'
+, 'NEWPORT'
+, 'N'
+, 15
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '27'
+, 'PEMBROKESHIRE'
+, 'N'
+, 16
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '6'
+, 'POWYS'
+, 'N'
+, 17
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '58'
+, 'RHONDDA CYNON TAFF'
+, 'N'
+, 18
+, NULL
+, NULL );
+
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '54'
+, 'SWANSEA'
+, 'N'
+, 19
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '35'
+, 'TORFAEN'
+, 'N'
+, 20
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '72'
+, 'VALE OF GLAMORGAN'
+, 'N'
+, 21
+, NULL
+, NULL );
+
+insert into hig_codes
+( hco_domain
+, hco_code
+, hco_meaning
+, hco_system
+, hco_seq
+, hco_start_date
+, hco_end_date)
+values
+( 'PLANAPPS_REGIONS'
+, '16'
+, 'WREXHAM'
+, 'N'
+, 22
+, NULL
+, NULL );
+
