5ba6955d47a57da076034b0fdc9598bd068be7c4
Ian.Turnbull <Ian.Turnbull@37afec09-bd54-7940-9fac-cf987e4a02e6>
2011-01-26 07:56:16 +0000

    Update to deal with exception found from aileen
    git-svn-id: svn://127.0.0.1/customer/trunk@675 37afec09-bd54-7940-9fac-cf987e4a02e6

--- General Scripts/BRS3225/set_all_null_srids.sql
+++ General Scripts/BRS3225/set_all_null_srids.sql
@@ -2,11 +2,11 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/customer/General Scripts/BRS3225/set_all_null_srids.sql-arc   3.0   Nov 16 2010 12:14:22   Ian.Turnbull  $
+--       pvcsid           : $Header:   //vm_latest/archives/customer/General Scripts/BRS3225/set_all_null_srids.sql-arc   3.1   Jan 26 2011 07:56:16   Ian.Turnbull  $
 --       Module Name      : $Workfile:   set_all_null_srids.sql  $
---       Date into PVCS   : $Date:   Nov 16 2010 12:14:22  $
---       Date fetched Out : $Modtime:   Nov 10 2010 18:17:44  $
---       PVCS Version     : $Revision:   3.0  $
+--       Date into PVCS   : $Date:   Jan 26 2011 07:56:16  $
+--       Date fetched Out : $Modtime:   Jan 18 2011 18:22:46  $
+--       PVCS Version     : $Revision:   3.1  $
 --       Based on SCCS version :
 --
 --   Author : Aileen Heal
@@ -27,19 +27,20 @@
 -- N.B. it is important after running this script to ** RESTART MAPVIEWER **
 --
 ---------------------------------------------------------------------------------------------------
+col diminfo format a30
+col column_name format a30
 
 col         log_extension new_value log_extension noprint
-select  TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.LOG' log_extension from dual
+select  instance_name||'_set_All_null_srids_'||TO_CHAR(sysdate,'DDMONYYYY_HH24MISS')||'.log' log_extension from v$instance
 /
 ---------------------------------------------------------------------------------------------------
 -- Spool to Logfile
-
-define logfile1='set_all_null_srids_&log_extension'
+define logfile1='&log_extension'
 set pages 0
 set lines 200
+set echo on
 SET SERVEROUTPUT ON size 1000000
 
-spool &logfile1
 
 set echo on
 
@@ -55,6 +56,11 @@ select user from dual
 select instance_name  from v$instance
 /
 
+
+-- select table_name,srid from user_Sdo_geom_metadata
+--order by 1;
+
+
 @AH_SET_TABLE_SRID.sql
 
 prompt
@@ -64,9 +70,10 @@ declare
    l_allow_debug    hig_options.HOP_VALUE%type;
    l_reg_sde_layer  hig_options.HOP_VALUE%type;
    l_default_srid      number := &pv_default_srid;
+   l_table_type     varchar2(40);
       
    cursor c1 is
-         select nth_theme_id,nth_theme_name, nth_feature_table, nth_feature_shape_column  
+/*         select nth_theme_id,nth_theme_name, nth_feature_table, nth_feature_shape_column  
            from nm_themes_all
         where nth_feature_table in
        (select table_name from user_sdo_geom_metadata where srid is null)
@@ -75,12 +82,110 @@ declare
                                    where object_name = nth_feature_table 
                                        and object_type =  'MATERIALIZED VIEW')
           ORDER BY nth_base_table_theme NULLS FIRST;
- 
+ */ 
+ select distinct nth_theme_id,nth_theme_name, nth_feature_table, nth_feature_shape_column,srid
+from
+(SELECT distinct nth_theme_id,nth_theme_name, nth_feature_table, nth_feature_shape_column,u.sdo_srid srid
+      FROM MDSYS.sdo_geom_metadata_table u,
+           (SELECT   hus_username, nth_feature_table, nth_feature_shape_column,nth_theme_id, nth_theme_name
+                FROM (SELECT hus_username, a.nth_feature_table, a.nth_feature_shape_column, nth_theme_id, nth_Theme_name
+                        FROM nm_themes_all a,
+                             nm_theme_roles,
+                             hig_user_roles,
+                             hig_users,
+                             all_users
+                       WHERE nthr_theme_id = a.nth_theme_id
+                         AND nthr_role = hur_role
+                         AND hur_username = hus_username
+                         AND hus_username = username
+                         AND hus_username != hig.get_application_owner
+                         AND hus_end_date IS NULL
+                         AND NOT EXISTS
+                         -- Ignore the TMA Webservice schemas
+                           (SELECT 1 FROM all_objects o
+                             WHERE o.object_name = 'GET_RECIPIENTS'
+                               AND o.object_type = 'PROCEDURE'
+                               AND o.owner = hus_username)
+                         -- Make sure the role is actually granted 
+                         AND EXISTS
+                           (SELECT * FROM dba_role_privs
+                             WHERE grantee = hus_username
+                               AND granted_role = nthr_role)
+                         AND NOT EXISTS (
+                                SELECT 1
+                                  FROM MDSYS.sdo_geom_metadata_table g1
+                                 WHERE g1.sdo_owner = hus_username
+                                   AND g1.sdo_table_name = nth_feature_table
+                                   AND g1.sdo_column_name = nth_feature_shape_column)
+                      UNION ALL
+                      SELECT hus_username, b.nth_feature_table, b.nth_feature_shape_column,b.nth_theme_id ,b.nth_theme_name
+                        FROM nm_themes_all a,
+                             hig_users,
+                             all_users,
+                             nm_themes_all b
+                       WHERE b.nth_theme_id = a.nth_base_table_theme
+                         AND hus_username = username
+                         AND hus_username != hig.get_application_owner
+                         AND hus_end_date IS NULL
+                         AND NOT EXISTS
+                         -- Ignore the TMA Webservice schemas
+                           (SELECT 1 FROM all_objects o
+                             WHERE o.object_name = 'GET_RECIPIENTS'
+                               AND o.object_type = 'PROCEDURE'
+                               AND o.owner = hus_username)
+                         AND NOT EXISTS (
+                                SELECT 1
+                                  FROM MDSYS.sdo_geom_metadata_table g1
+                                 WHERE g1.sdo_owner = hus_username
+                                   AND g1.sdo_table_name = b.nth_feature_table
+                                   AND g1.sdo_column_name = b.nth_feature_shape_column)
+                         -- Make sure we only report base table themes that we actually
+                         -- have the roles to access their child View based themes
+                         AND a.nth_theme_id IN (
+                                SELECT z.nth_theme_id
+                                  FROM nm_themes_all z, nm_theme_roles
+                                 WHERE EXISTS (SELECT 1
+                                                 FROM hig_user_roles
+                                                WHERE hur_username = hus_username 
+                                                  AND hur_role = nthr_role)
+                                   AND nthr_theme_id = z.nth_theme_id))
+            GROUP BY hus_username, nth_feature_table, nth_feature_shape_column,nth_theme_id,nth_theme_name)
+     WHERE u.sdo_table_name = nth_feature_table
+       AND u.sdo_column_name = nth_feature_shape_column
+       AND u.sdo_owner = hig.get_application_owner
+ union all
+    select
+     nth_theme_id,nth_theme_name, nth_feature_table, nth_feature_shape_column,null
+      FROM nm_themes_all
+     WHERE nth_theme_type = 'SDO'
+       AND NOT EXISTS (
+              SELECT 1
+                FROM user_sdo_geom_metadata
+               WHERE table_name = nth_feature_table
+                 AND column_name = nth_feature_shape_column)
+union all
+ select nth_theme_id,nth_theme_name, nth_feature_table, nth_feature_shape_column  ,null
+           from nm_themes_all
+        where nth_feature_table in
+       (select table_name from user_sdo_geom_metadata where srid is null)
+           and exists ( select 1 from user_tables where  table_name = nth_feature_table )
+           and not exists ( select 1 from user_objects 
+                                   where object_name = nth_feature_table 
+                                       and object_type =  'MATERIALIZED VIEW'))
+  where nth_feature_table is not null                                   
+order by 1;         
+          
+          
    cursor c2 is
          Select table_name, column_name 
            from user_sdo_geom_metadata
         where table_name in ( 'TMA_NONSWA_STR_LOCS_SDO',   'TMA_SW_LICENCES_SDO')
             and srid is null;
+
+   cursor c3 (pi_table nm_themes_all.nth_feature_table%type) is
+   	    select object_type from all_objects
+   	    where object_name=pi_table
+   	     and owner=hig.get_application_owner;
 
 begin
 
@@ -93,19 +198,29 @@ begin
  
   for c1rec in c1 loop
     -- first check it is not a materalised view
-    
-    nm_debug.debug('Setting SRID for ' || c1rec.nth_feature_table|| ' to  '|| l_default_srid );
-    ah_set_table_srid(c1rec.nth_feature_table, c1rec.nth_feature_shape_column, l_default_srid );
-     
-    nm_debug.debug('Refreshing SDO metadata for ' || c1rec.nth_feature_table  );
-    nm3layer_tool.refresh_sdo_metadata( c1rec.nth_theme_id, 'ALL_DATA');
+--check is a table, not view or MV and that the srid is null 
+    open c3(c1rec.nth_feature_Table);
+    fetch c3 into l_table_type;
+    close c3; 
+    nm_debug.debug(c1rec.nth_feature_table||' '||l_table_type||' SRID '|| c1rec.srid );
+    if l_table_type = 'TABLE' and c1rec.srid is null then
+      nm_debug.debug('Setting SRID for ' || c1rec.nth_feature_table|| ' to  '|| l_default_srid );
+      ah_set_table_srid(c1rec.nth_feature_table, c1rec.nth_feature_shape_column, l_default_srid );
+    end if;  
+--end check     
 
+/* -- remove to seperate procedure
+    nm_debug.debug('Refreshing SDO metadata for ' || c1rec.nth_feature_table  );
+    nm3layer_tool.refresh_sdo_metadata( c1rec.nth_theme_id);--, 'ALL_DATA');
      if (l_reg_sde_layer = 'Y')  then
         nm_debug.debug('Refreshing SDE metadata for ' || c1rec.nth_feature_table );
-        nm3layer_tool.refresh_sde_metadata( c1rec.nth_theme_id, 'ALL_DATA');
+        nm3layer_tool.refresh_sde_metadata( c1rec.nth_theme_id);--, 'ALL_DATA');
      end if;
+*/
 
+     commit;
   end loop;
+  nm_debug.debug('Checking other TMA tables ');
 
   -- do the 2 TMA tables which for some reason are not in nm_themes_all
      for c2rec in c2 loop 
