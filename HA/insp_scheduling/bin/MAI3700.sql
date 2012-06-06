9176e274df1eb51080bc231d6e9fa08ce002ba40
Ian.Turnbull <Ian.Turnbull@37afec09-bd54-7940-9fac-cf987e4a02e6>
2012-06-06 15:22:34 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@851 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ HA/insp_scheduling/bin/MAI3700.sql
@@ -0,0 +1,141 @@
+-----------------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       pvcsid                 : $Header:   //vm_latest/archives/customer/HA/insp_scheduling/bin/MAI3700.sql-arc   1.0   Jun 06 2012 16:22:34   Ian.Turnbull  $
+--       Module Name      : $Workfile:   MAI3700.sql  $
+--       Date into PVCS   : $Date:   Jun 06 2012 16:22:34  $
+--       Date fetched Out : $Modtime:   Jun 06 2012 14:30:38  $
+--       PVCS Version     : $Revision:   1.0  $
+--
+--
+--   Author : P Stanton
+--
+-----------------------------------------------------------------------------
+--    Copyright (c) Bentely ltd, 2012
+-----------------------------------------------------------------------------
+exec higgrirp.write_gri_spool(&1,'Highways by Exor');
+exec higgrirp.write_gri_spool(&1,'================');
+exec higgrirp.write_gri_spool(&1,'');
+exec higgrirp.write_gri_spool(&1,'Working ....');
+
+set ver off
+set feed off
+col spool new_value spool noprint;
+
+select higgrirp.get_module_spoolpath(&1,user)||higgrirp.get_module_spoolfile(&1) spool 
+from dual;
+
+spool &spool
+
+prompt
+
+prompt Generate Inspections by Interval
+prompt ================================
+prompt
+prompt 
+prompt Generate Inspections by Interval
+prompt
+prompt Submitting ....
+prompt
+declare
+   
+   l_asset_type          varchar2(255);
+   l_insp_type           varchar2(255);
+   l_region_of_interest  varchar2(255);
+   l_from_date           DATE;
+   l_to_date             DATE;
+   l_error                varchar2(255);
+   l_gri_id               number;
+  
+-----------------------------------------
+   l_process_type_id           hig_processes.hp_process_type_id%TYPE;
+   l_initiated_by_username     hig_processes.hp_initiated_by_username%TYPE;
+   l_initiated_date            hig_processes.hp_initiated_date%TYPE;
+   l_initiators_ref            hig_processes.hp_initiators_ref%TYPE;
+   l_start_date                date;
+   l_frequency_id              hig_processes.hp_frequency_id%TYPE;
+   l_polling_flag              hig_processes.hp_polling_flag%TYPE;
+   l_area_id                   hig_processes.hp_area_id%TYPE;
+   l_check_file_cardinality    BOOLEAN;
+   l_process_id                hig_processes.hp_process_id%TYPE;
+   l_job_name                  hig_processes.hp_job_name%TYPE;
+   l_scheduled_start_date      DATE;   
+----------------------------------
+
+   l_params        hig_process_api.rec_temp_params;
+
+begin
+
+   dbms_output.enable;
+
+   select grr_job_id into l_gri_id from  gri_report_runs
+   where grr_job_id = &1;
+ 
+   l_process_type_id := -5001;
+   l_initiated_by_username := user;
+   l_initiated_date := sysdate;
+   l_initiators_ref  := null;
+   l_start_date := sysdate;
+   l_frequency_id := -1;
+   l_polling_flag := 'N';
+   l_area_id    := null;
+   l_check_file_cardinality := FALSE;
+
+   hig_process_api.initialise_temp_params;
+
+   l_params.param_name := 'GRI_ID';
+   l_params.param_value := l_gri_id;
+   hig_process_api.add_temp_param (l_params);
+   
+   l_params.param_name := 'MODULE';
+   l_params.param_value := 'MAI3700';
+   hig_process_api.add_temp_param (l_params);
+  
+
+   hig_process_api.create_and_schedule_process    (pi_process_type_id           => l_process_type_id
+                                                 , pi_initiated_by_username     => l_initiated_by_username
+                                                 , pi_initiated_date            => l_initiated_date
+                                                 , pi_initiators_ref            => l_initiators_ref
+                                                 , pi_start_date                => l_start_date
+                                                 , pi_frequency_id              => l_frequency_id
+                                                 , pi_polling_flag              => l_polling_flag
+                                                 , pi_area_id                   => l_area_id
+                                                 , pi_check_file_cardinality    => l_check_file_cardinality
+                                                 , po_process_id                => l_process_id
+                                                 , po_job_name                  => l_job_name
+                                                 , po_scheduled_start_date      => l_scheduled_start_date);
+
+
+   higgrirp.write_gri_spool(&1,'Schedule has been submitted to Process Manager.');
+   higgrirp.write_gri_spool(&1,'Please use the Process Monitor module to view details. Process ID: '||l_process_id);
+
+end;
+/
+set define on
+set term off
+set head off
+
+spool off
+
+spool run_file.sql
+select 'Prompt Press Return to exit '||chr(10)||' pause'
+from gri_report_runs
+where grr_job_id = '&1'
+and grr_mode != 'WEB'
+/
+
+exec higgrirp.write_gri_spool(&1,'Finished');
+
+update gri_report_runs 
+set grr_end_date = sysdate,
+grr_error_no = 0,
+grr_error_descr = 'Normal Successful Completion'
+where grr_job_id = &&1; 
+
+set term on
+set define off
+spool off
+start run_file
+
+exit;
