560d2ca1bb50a7328af151aa1b71950ad80b5b86
Ian.Turnbull <Ian.Turnbull@37afec09-bd54-7940-9fac-cf987e4a02e6>
2012-06-26 11:29:40 +0000

    wol theme parameters
    git-svn-id: svn://127.0.0.1/customer/trunk@886 37afec09-bd54-7940-9fac-cf987e4a02e6

--- tfl/IM work Order Defects on Map/wo_defects_on_map.js
+++ tfl/IM work Order Defects on Map/wo_defects_on_map.js
@@ -2,11 +2,11 @@
  --
  --   PVCS Identifiers :-
  --
- --       pvcsid           : $Header:   //vm_latest/archives/customer/tfl/IM work Order Defects on Map/wo_defects_on_map.js-arc   1.2   Jun 26 2012 10:59:14   Ian.Turnbull  $
+ --       pvcsid           : $Header:   //vm_latest/archives/customer/tfl/IM work Order Defects on Map/wo_defects_on_map.js-arc   1.3   Jun 26 2012 12:29:40   Ian.Turnbull  $
  --       Module Name      : $Workfile:   wo_defects_on_map.js  $
- --       Date into PVCS   : $Date:   Jun 26 2012 10:59:14  $
- --       Date fetched Out : $Modtime:   Jun 26 2012 10:58:46  $
- --       PVCS Version     : $Revision:   1.2  $
+ --       Date into PVCS   : $Date:   Jun 26 2012 12:29:40  $
+ --       Date fetched Out : $Modtime:   Jun 26 2012 12:29:16  $
+ --       PVCS Version     : $Revision:   1.3  $
  --       Based on SCCS version :
  --
  --
@@ -17,7 +17,7 @@
 function addTheme(pLayerName, pFeatureID){
    var xthemebasedfoi = new MVThemeBasedFOI('locator', dataSource + '.' + pLayerName);
    if (pLayerName == 'IM_WORK_ORDER_LINES'){
-    xthemebasedfoi.setQueryParameters( pFeatureID, pFeatureID, pFeatureID );
+    xthemebasedfoi.setQueryParameters( '~', '~', pFeatureID );
    } else {
     xthemebasedfoi.setQueryParameters(pFeatureID);
    } 
