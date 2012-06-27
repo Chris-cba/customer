475123fc2c5c5e2be6b1327b18bb51fbf20cbd45
Ian.Turnbull <Ian.Turnbull@37afec09-bd54-7940-9fac-cf987e4a02e6>
2012-06-27 09:15:36 +0000

    changed feature name from locator to the layer name
    git-svn-id: svn://127.0.0.1/customer/trunk@888 37afec09-bd54-7940-9fac-cf987e4a02e6

--- tfl/IM work Order Defects on Map/wo_defects_on_map.js
+++ tfl/IM work Order Defects on Map/wo_defects_on_map.js
@@ -2,11 +2,11 @@
  --
  --   PVCS Identifiers :-
  --
- --       pvcsid           : $Header:   //vm_latest/archives/customer/tfl/IM work Order Defects on Map/wo_defects_on_map.js-arc   1.3   Jun 26 2012 12:29:40   Ian.Turnbull  $
+ --       pvcsid           : $Header:   //vm_latest/archives/customer/tfl/IM work Order Defects on Map/wo_defects_on_map.js-arc   1.4   Jun 27 2012 10:15:36   Ian.Turnbull  $
  --       Module Name      : $Workfile:   wo_defects_on_map.js  $
- --       Date into PVCS   : $Date:   Jun 26 2012 12:29:40  $
- --       Date fetched Out : $Modtime:   Jun 26 2012 12:29:16  $
- --       PVCS Version     : $Revision:   1.3  $
+ --       Date into PVCS   : $Date:   Jun 27 2012 10:15:36  $
+ --       Date fetched Out : $Modtime:   Jun 27 2012 10:15:04  $
+ --       PVCS Version     : $Revision:   1.4  $
  --       Based on SCCS version :
  --
  --
@@ -15,7 +15,7 @@
  -----------------------------------------------------------------------------
  */
 function addTheme(pLayerName, pFeatureID){
-   var xthemebasedfoi = new MVThemeBasedFOI('locator', dataSource + '.' + pLayerName);
+   var xthemebasedfoi = new MVThemeBasedFOI(pLayerName, dataSource + '.' + pLayerName);
    if (pLayerName == 'IM_WORK_ORDER_LINES'){
     xthemebasedfoi.setQueryParameters( '~', '~', pFeatureID );
    } else {
