062771503b25ebff96ec8e095e564b855ca8d3a0
Ian.Turnbull <Ian.Turnbull@37afec09-bd54-7940-9fac-cf987e4a02e6>
2013-11-27 13:17:18 +0000

    update from aileen
    git-svn-id: svn://127.0.0.1/customer/trunk@939 37afec09-bd54-7940-9fac-cf987e4a02e6

--- Virgin/gis scripts/refresh_virgins_mviews.sql
+++ Virgin/gis scripts/refresh_virgins_mviews.sql
@@ -2,26 +2,28 @@
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.1   Jun 20 2012 09:21:48   Ian.Turnbull  $
+--       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.2   Nov 27 2013 13:17:18   Ian.Turnbull  $
 --       Module Name      : $Workfile:   refresh_virgins_mviews.sql  $
---       Date into PVCS   : $Date:   Jun 20 2012 09:21:48  $
---       Date fetched Out : $Modtime:   Jun 20 2012 08:14:34  $
---       PVCS Version     : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Nov 27 2013 13:17:18  $
+--       Date fetched Out : $Modtime:   Nov 26 2013 16:28:44  $
+--       PVCS Version     : $Revision:   1.2  $
 --       Based on SCCS version :
 --
 --   Author : Aileen Heal
 --
 ---------------------------------------------------------------------------------------------------
---    Copyright (c) exor corporation ltd, 2012
+--    Copyright (c) Bentley Systems (UK) Ltd 2013
 ---------------------------------------------------------------------------------------------------
 --
---  this script will drop and recreate the Vrigin NSG Materalised views. 
+-- This script will drop and recreate the Virgin NSG Materalised views. 
 --
--- updated 14th march 2011 by Aileen heal to drop the spatial indexes before dropping the materalised views 
+-- Updated 15th October 2013 to use different views and to remove unused materalized view
+--
+-- Updated 14th march 2011 by Aileen Heal to drop the spatial indexes before dropping the materalised views 
 -- to overcome the error ORA-04020 When Dropping Materialized View with a Spatial Index
 -- see http://www.oracledistilled.com/oracle-database/troubleshooting/ora-04020-when-dropping-materialized-view-with-a-spatial-index/
 --
--- updated 31st March 2011 to includ PVCS header in view definition.
+-- Updated 31st March 2011 to include PVCS header in view definition.
 ---------------------------------------------------------------------------------------------------
 
 col         log_extension new_value log_extension noprint
@@ -51,25 +53,20 @@ TABLESPACE UKGAZ_DATA as
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.1   Jun 20 2012 09:21:48   Ian.Turnbull  $
+--       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.2   Nov 27 2013 13:17:18   Ian.Turnbull  $
 --       Module Name      : $Workfile:   refresh_virgins_mviews.sql  $
---       Date into PVCS   : $Date:   Jun 20 2012 09:21:48  $
---       Date fetched Out : $Modtime:   Jun 20 2012 08:14:34  $
---       PVCS Version     : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Nov 27 2013 13:17:18  $
+--       Date fetched Out : $Modtime:   Nov 26 2013 16:28:44  $
+--       PVCS Version     : $Revision:   1.2  $
 --       Based on SCCS version :
 --
 --   Author : Aileen Heal
 --
--- write to create materalised view for Virgin's theme TYPE 21
+-- Written to create materalised view for Virgin's theme TYPE 21
 ---------------------------------------------------------------------------------------------------
---    Copyright (c) exor corporation ltd, 2012
+--    Copyright (c) Bentley Systems (UK) Ltd 2013
 ---------------------------------------------------------------------------------------------------
-          i.TP21_IIT_NE_ID,
-          i.TP21_ROAD_STATUS,
-          s.objectid,
-          s.geoloc
-     FROM v_nm_nsg_asd_tp21 i, nm_nit_tp21_sdo s
-    WHERE i.tp21_iit_ne_id = s.ne_id;
+     *      FROM V_NM_NIT_TP21_SDO_DT;
 
 
 COMMENT ON MATERIALIZED VIEW MV_TYPE_21_LOCATOR IS 'Bespoke materialised view created by Aileen Heal for Virgin Media';
@@ -103,24 +100,21 @@ TABLESPACE UKGAZ_DATA as
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.1   Jun 20 2012 09:21:48   Ian.Turnbull  $
+--       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.2   Nov 27 2013 13:17:18   Ian.Turnbull  $
 --       Module Name      : $Workfile:   refresh_virgins_mviews.sql  $
---       Date into PVCS   : $Date:   Jun 20 2012 09:21:48  $
---       Date fetched Out : $Modtime:   Jun 20 2012 08:14:34  $
---       PVCS Version     : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Nov 27 2013 13:17:18  $
+--       Date fetched Out : $Modtime:   Nov 26 2013 16:28:44  $
+--       PVCS Version     : $Revision:   1.2  $
 --       Based on SCCS version :
 --
 --   Author : Aileen Heal
 --
--- write to create materalised view for Virgin's theme TYPE 22
+-- Written to create materalised view for Virgin's theme TYPE 22
 ---------------------------------------------------------------------------------------------------
---    Copyright (c) exor corporation ltd, 2012
+--    Copyright (c) Bentley Systems (UK) Ltd 2013
 ---------------------------------------------------------------------------------------------------
-          i.TP22_IIT_NE_ID,
-          i.TP22_REINSTATEMENT,
-          s.objectid, s.geoloc
-     FROM v_nm_nsg_asd_tp22 i, nm_nit_tp22_sdo s
-    WHERE i.tp22_iit_ne_id = s.ne_id; 
+     *      FROM V_NM_NIT_TP22_SDO_DT;
+
 
 COMMENT ON MATERIALIZED VIEW MV_TYPE_22_LOCATOR IS 'Bespoke materialised view created by Aileen Heal for Virgin Media';
 
@@ -153,24 +147,20 @@ TABLESPACE UKGAZ_DATA as
 --
 --   PVCS Identifiers :-
 --
---       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.1   Jun 20 2012 09:21:48   Ian.Turnbull  $
+--       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.2   Nov 27 2013 13:17:18   Ian.Turnbull  $
 --       Module Name      : $Workfile:   refresh_virgins_mviews.sql  $
---       Date into PVCS   : $Date:   Jun 20 2012 09:21:48  $
---       Date fetched Out : $Modtime:   Jun 20 2012 08:14:34  $
---       PVCS Version     : $Revision:   1.1  $
+--       Date into PVCS   : $Date:   Nov 27 2013 13:17:18  $
+--       Date fetched Out : $Modtime:   Nov 26 2013 16:28:44  $
+--       PVCS Version     : $Revision:   1.2  $
 --       Based on SCCS version :
 --
 --   Author : Aileen Heal
 --
--- write to create materalised view for Virgin's theme TYPE 23
+-- Written to create materalised view for Virgin's theme TYPE 23
 ---------------------------------------------------------------------------------------------------
---    Copyright (c) exor corporation ltd, 2012
+--    Copyright (c) Bentley Systems (UK) Ltd 2013
 ---------------------------------------------------------------------------------------------------
-          i.TP23_IIT_NE_ID,
-          i.TP23_DESIG_CODE, 
-          s.objectid, s.geoloc
-     FROM v_nm_nsg_asd_tp23 i, nm_nit_tp23_sdo s
-    WHERE i.tp23_iit_ne_id = s.ne_id;
+     *      FROM V_NM_NIT_TP23_SDO_DT;
 
 COMMENT ON MATERIALIZED VIEW MV_TYPE_23_LOCATOR IS 'Bespoke materialised view created by Aileen Heal for Virgin Media';
 
@@ -182,7 +172,7 @@ create unique index MV_TYPE_23_LOCATOR_INX
    NOLOGGING;
 
 create index MV_TYPE_23_LOCATOR_NE_INX 
-   on MV_TYPE_23_LOCATOR(TP23_IIT_NE_ID) 
+   on MV_TYPE_23_LOCATOR(IIT_NE_ID) 
    TABLESPACE UKGAZ_INDEX
    NOLOGGING;
 
@@ -190,61 +180,6 @@ create index MV_TYPE_23_LOCATOR_SPIDX on
    MV_TYPE_23_LOCATOR( GEOLOC ) 
    INDEXTYPE IS MDSYS.SPATIAL_INDEX
    PARAMETERS('sdo_indx_dims=2 Tablespace = UKGAZ_INDEX');
-
-
--- *********************** MV_TYPE_21_LOCATOR_ORG_1 ****************************
-
-DROP INDEX MV_TYPE_21_LOCATOR_ORG_1_SPIDX;
-
-DROP MATERIALIZED VIEW MV_TYPE_21_LOCATOR_ORG_1;
-
-CREATE MATERIALIZED VIEW MV_TYPE_21_LOCATOR_ORG_1
-TABLESPACE UKGAZ_DATA as
-   SELECT 
---------------------------------------------------------------------------------------------------
---
---   PVCS Identifiers :-
---
---       pvcsid           : $Header:   //vm_latest/archives/customer/Virgin/gis scripts/refresh_virgins_mviews.sql-arc   1.1   Jun 20 2012 09:21:48   Ian.Turnbull  $
---       Module Name      : $Workfile:   refresh_virgins_mviews.sql  $
---       Date into PVCS   : $Date:   Jun 20 2012 09:21:48  $
---       Date fetched Out : $Modtime:   Jun 20 2012 08:14:34  $
---       PVCS Version     : $Revision:   1.1  $
---       Based on SCCS version :
---
---   Author : Aileen Heal
---
--- write to create materalised view for Virgin's theme TYPE 21 ORG 1
----------------------------------------------------------------------------------------------------
---    Copyright (c) exor corporation ltd, 2012
----------------------------------------------------------------------------------------------------
-          i.TP21_IIT_NE_ID,
-          i.TP21_ORG_TYPE,
-          s.objectid,
-          s.geoloc
-     FROM v_nm_nsg_asd_tp21 i, nm_nit_tp21_sdo s
-    WHERE i.tp21_iit_ne_id = s.ne_id
-    AND i.tp21_org_type = '1';
-
-COMMENT ON MATERIALIZED VIEW MV_TYPE_21_LOCATOR_ORG_1 IS 'Bespoke materialised view created by Jonny Heald for Virgin Media';
-
-CREATE UNIQUE INDEX MV_TYPE_21_LOCATOR_ORG_1_IDX 
-   ON MV_TYPE_21_LOCATOR_ORG_1(OBJECTID) 
-   TABLESPACE UKGAZ_INDEX
-   NOLOGGING;
-
-CREATE INDEX MV_TYPE_21_LOCATOR_ORG_1_SPIDX 
-   ON MV_TYPE_21_LOCATOR_ORG_1(GEOLOC)
-   INDEXTYPE IS MDSYS.SPATIAL_INDEX
-   PARAMETERS('sdo_indx_dims=2 Tablespace = UKGAZ_INDEX');
-
-CREATE INDEX MV_TYPE_21_LOC_ORG_1_NE_IDX 
-   ON MV_TYPE_21_LOCATOR_ORG_1(TP21_IIT_NE_ID)
-   TABLESPACE UKGAZ_INDEX
-   NOLOGGING;
-
-commit;
-/
 
 -- *********************** All Done ****************************
 
