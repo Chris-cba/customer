3c1bf418627e34b4ac7c6cf15f5081e17af45d51
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 18:09:56 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1651 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Brisbane/BRAMS to SAP View Extracts/Version_01/admin/sql/procedures/XBCC_CONTEXT_DATES.sql
@@ -0,0 +1,7 @@
+create or replace procedure XBCC_CONTEXT_DATES ( pname  VARCHAR2, pvalue VARCHAR2) IS
+	BEGIN
+	  -- Create a session with a previously defined context.
+	  DBMS_SESSION.SET_CONTEXT('XBCC_DATES',pname,pvalue);
+END XBCC_CONTEXT_DATES;
+
+CREATE OR REPLACE CONTEXT XBCC_DATES USING XBCC_CONTEXT_DATES;
