a705cd9f1d25d2457156580cf116d7ac67471dc6
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:03:16 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1290 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-METP/Sent/2013-07-18_Installer - Copy/install/_updates.sql
@@ -0,0 +1,91 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\Hath.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\Hath.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_META_Insert.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_META_Insert.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_META_Insert.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_META_Insert.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_hath_fk.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_hath_fk.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\HATH_TRIG.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\HATH_TRIG.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\HATH_TRIG_insert.SQL ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\HATH_TRIG_insert.SQL
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\HATT_TRIG.SQL ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\HATT_TRIG.SQL
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\HATT_TRIG_insert.SQL ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\HATT_TRIG_insert.SQL
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\Hath_CSV_LOAD.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\Hath_CSV_LOAD.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\Hatt_CSV_LOAD.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\Hatt_CSV_LOAD.sql
+SET FEEDBACK OFF
+--
+--
