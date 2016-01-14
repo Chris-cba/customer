967ab5add3ca1309c33696185dec223c639d3f85
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 18:25:22 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1745 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Larimer County/Pod Updates/new  2.sql
@@ -0,0 +1,31 @@
+update user_sdo_themes
+set styling_rules = 
+'<?xml version="1.0" standalone="yes"?> '||
+'<styling_rules> '||
+'  <hidden_info> '||
+'    <field column="ROAD_NETWORK_SDO_ID" name="IM_NETWORK"/> '||
+'  </hidden_info> '||
+'  <rule> '||
+'    <features style="L.MAJOR STREET"> ( '||
+'road_network_sdo_id IN ( SELECT NE_ID FROM NM_ELEMENTS WHERE NE_ID = NM3NET.GET_NE_ID(:1) AND ne_type = ''S'') '||
+'OR '||
+'( '||
+'road_network_sdo_id in '||
+'  (SELECT '||
+'              nm_ne_id_of data '||
+'              FROM '||
+'              nm_members '||
+'             WHERE  '||
+'              nm3net.get_ne_gty( nm_ne_id_of ) IS   '||
+'              NULL CONNECT BY PRIOR nm_ne_id_of = nm_ne_id_in  '||
+'             AND  '||
+'              nm3net.get_ne_gty( nm_ne_id_of ) IS NULL   '||
+'             START '||
+'             WITH NM_NE_ID_IN                     = (select nm_ne_id_in from nm_members where nm_obj_type = ''CRTE'' and nm_type = ''G'' and nm_ne_id_of  = NM3NET.GET_NE_ID(:2)) '||
+'           ))) </features> '||
+'    <label column="NE_UNIQUE_IM_LABEL" style="T.CITY NAME"> 1 </label> '||
+'  </rule> '||
+'</styling_rules>'
+where name = 'IM_NETWORK';
+
+commit;
