add7e04cb50125ccbfdb1b60b70012dabe383743
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 19:08:22 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@2004 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ RMS-RAMS/Reflect With Insight/version_x/Views/_updates.sql
@@ -0,0 +1,101 @@
+
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\Create_Asset_Themes.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\Create_Asset_Themes.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\requestss_during_a_period_sdo.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\requestss_during_a_period_sdo.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\xrms_context_rpt_dates.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\xrms_context_rpt_dates.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\accomplishments_during_a_period.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\accomplishments_during_a_period.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\accomplishments_during_a_period_sdo.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\accomplishments_during_a_period_sdo.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\defects_during_a_period.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\defects_during_a_period.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\defects_during_a_period_sdo.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\defects_during_a_period_sdo.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\inspections_during_a_period.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\inspections_during_a_period.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\inspections_during_a_period_sdo.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\inspections_during_a_period_sdo.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\performance_during_a_selected_period.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\performance_during_a_selected_period.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\sql\requestss_during_a_period.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\sql\requestss_during_a_period.sql
+SET FEEDBACK OFF
+--
+--
