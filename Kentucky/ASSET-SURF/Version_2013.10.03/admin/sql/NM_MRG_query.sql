c202281170118bdaefadf039a04a2cbbbc878a4c
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:32:12 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1378 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-SURF/Version_2013.10.03/admin/sql/NM_MRG_query.sql
@@ -0,0 +1,114 @@
+
+
+declare
+
+
+l_merge_id number;
+
+
+begin
+    begin
+    select nmq_id into l_merge_id from NM_MRG_QUERY where NMQ_UNIQUE = 'MQ_SURF';
+    
+    delete from  NM_MRG_QUERY where NMQ_ID = l_merge_id;
+    
+    exception
+    when others then null;
+    
+    end;
+    
+    commit;  
+
+    select NMQ_ID_SEQ.nextval into l_merge_id from dual;
+    
+    Insert into NM_MRG_QUERY
+       (NMQ_ID, NMQ_UNIQUE, NMQ_DESCR, NMQ_INNER_OUTER_JOIN, NMQ_INV_TYPE_X_SECT_COUNT, NMQ_TRANSIENT_QUERY)
+    select 
+       l_merge_id
+       , 'MQ_SURF'
+       , 'MQ_SURF'
+       , 'O'
+       , 0
+       , 'Y' from dual;
+
+     commit;
+
+       Insert into NM_MRG_QUERY_ROLES
+           (NQRO_NMQ_ID, NQRO_ROLE, NQRO_MODE)
+               select
+            l_merge_id
+           , 'HIG_ADMIN'
+           , 'NORMAL'
+           from dual;
+
+        commit;
+
+        -- NM_MRG_QUERY_TYPES - asset types 
+        
+        
+
+             
+	
+        Insert into NM_MRG_QUERY_TYPES
+           (NQT_NMQ_ID, NQT_SEQ_NO, NQT_INV_TYPE, NQT_DEFAULT)
+           select 
+               l_merge_id
+           , NQT_SEQ_NO_SEQ.nextval
+           , nit_inv_type
+           , 'N'
+        from
+        nm_inv_types where nit_inv_type = 'PM' 	;
+			 
+		        Insert into NM_MRG_QUERY_TYPES
+           (NQT_NMQ_ID, NQT_SEQ_NO, NQT_INV_TYPE, NQT_DEFAULT)
+           select 
+               l_merge_id
+           , NQT_SEQ_NO_SEQ.nextval
+           , nit_inv_type
+           , 'N'
+        from
+        nm_inv_types where nit_inv_type = 'BD' 	;	 
+
+		commit;
+		
+           		   
+        -- NM_MRG_QUERY_ATTRIBS asset attributes
+
+        INSERT INTO NM_MRG_QUERY_ATTRIBS (NQA_NMQ_ID, NQA_NQT_SEQ_NO, NQA_ATTRIB_NAME) 
+        select NQT_NMQ_ID 
+             , NQT_SEQ_NO
+             , ita_attrib_name
+      -- , nvl(ITA_VIEW_ATTRI,'IIT_PRIMARY_KEY')
+            from nm_inv_type_attribs,
+        NM_MRG_QUERY_TYPES,
+        NM_MRG_QUERY
+        where ita_inv_type(+) = NQT_INV_TYPE
+        and NQT_NMQ_ID = NMQ_ID
+        -- and NQT_NMQ_ID = 39
+        and NQT_NMQ_ID = l_merge_id
+        AND ITA_scrn_text = 'Surface Type'
+		and NQT_INV_TYPE = 'BD'
+       ;
+      
+             INSERT INTO NM_MRG_QUERY_ATTRIBS (NQA_NMQ_ID, NQA_NQT_SEQ_NO, NQA_ATTRIB_NAME) 
+        select NQT_NMQ_ID 
+             , NQT_SEQ_NO
+             , ita_attrib_name
+      -- , nvl(ITA_VIEW_ATTRI,'IIT_PRIMARY_KEY')
+            from nm_inv_type_attribs,
+        NM_MRG_QUERY_TYPES,
+        NM_MRG_QUERY
+        where ita_inv_type(+) = NQT_INV_TYPE
+        and NQT_NMQ_ID = NMQ_ID
+        -- and NQT_NMQ_ID = 39
+        and NQT_NMQ_ID = l_merge_id
+        AND ITA_scrn_text = 'PM Pavement Type'
+		and NQT_INV_TYPE= 'PM'
+       ;
+     
+        -- build the merge views so that the building of the derived asset works:
+        commit;
+     
+      NM3MRG_VIEW.build_view(l_merge_ID);
+END;
+/
