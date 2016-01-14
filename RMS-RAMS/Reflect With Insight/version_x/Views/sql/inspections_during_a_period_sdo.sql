6c7c9c2ca41f13d27a93fc79ac9a4698470e75cb
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 19:09:06 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@2011 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ RMS-RAMS/Reflect With Insight/version_x/Views/sql/inspections_during_a_period_sdo.sql
@@ -0,0 +1,36 @@
+create or replace view x_rms_rsd_idap_sdo as
+select 	
+	RSD.IIT_CHR_ATTRIB26	Vendor_Code
+,	RSD.IIT_NUM_ATTRIB25	Reference_id
+,	RSD.IIT_CHR_ATTRIB56	Road_Number
+,	RSD.IIT_CHR_ATTRIB28	Road_Maintenance_Segment
+,	RSD.IIT_DATE_ATTRIB86	Date_of_creation
+,	RSD.IIT_NUM_ATTRIB17	Longitude
+,	RSD.IIT_NUM_ATTRIB18	Latitude
+,	RSD.IIT_CHR_ATTRIB29	LGA
+,	RSD.IIT_CHR_ATTRIB27	Asset_type_code
+,	RSD.IIT_NUM_ATTRIB16	Key_ID
+,	RSD.IIT_CHR_ATTRIB58	Asset_description
+,	RSIN.IIT_NUM_ATTRIB16	Inspection_ID
+,	RSIN.IIT_CHR_ATTRIB27	Inspection_Type
+,	RSIN.IIT_DATE_ATTRIB88	Inspection_Completion_Date
+,	RSIN.IIT_DATE_ATTRIB89	Inspection_Completion_Time
+,	RSIN.IIT_CHR_ATTRIB28	Inspection_Number
+,	RSIN.IIT_CHR_ATTRIB66	Inspection_Comments
+,	ele.member_unique		ne_unique
+, 	ele.ne_descr			ne_descr
+,	sdo.objectid	Objectid
+,	geoloc
+from 	
+nm_inv_items RSD	
+,(select * from nm_inv_items where IIT_INV_TYPE = 'RSIN') RSIN	
+, (select distinct iit_ne_id,  member_unique, ne_descr from v_nm_rsd_nw, nm_elements where ne_unique = member_unique) ele
+, V_NM_NIT_RSD_SDO sdo
+where 1=1--	
+and RSD.IIT_INV_TYPE = 'RSD'	
+--	
+--	
+and rsd.iit_ne_id = sdo.ne_id 
+and rsd.iit_primary_key = rsin.iit_foreign_key
+and rsd.iit_ne_id = ele.iit_ne_id(+)
+;
