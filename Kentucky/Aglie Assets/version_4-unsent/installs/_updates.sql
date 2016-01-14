81610ade1b8a19f31bddb49b3dc4ae1088f72616
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 15:29:06 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1122 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/Aglie Assets/version_4-unsent/installs/_updates.sql
@@ -0,0 +1,137 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\tables\xaa_route_temp.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\tables\xaa_route_temp.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\tables\XAA_SPATIAL_AUDIT.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\tables\XAA_SPATIAL_AUDIT.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\tables\xaa_asset_type.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\tables\xaa_asset_type.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\tables\xaa_length_change.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\tables\xaa_length_change.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\tables\xaa_loc_ident_net_ref.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\tables\xaa_loc_ident_net_ref.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\views\xaa_route_sdo.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\views\xaa_route_sdo.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\views\xaa_route.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\views\xaa_route.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\views\xaa_route_all.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\views\xaa_route_all.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\views\xaa_route_all_recal.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\views\xaa_route_all_recal.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\triggers\xaa_SPATIAL_AUDIT_trg.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\triggers\xaa_SPATIAL_AUDIT_trg.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\x_log_table.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\x_log_table.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\x_log_table.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\x_log_table.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xky_hig_to_aa.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xky_hig_to_aa.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xky_hig_to_aa.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xky_hig_to_aa.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xky_hig_to_aa.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xky_hig_to_aa.sql
+SET FEEDBACK OFF
+--
+--
+
