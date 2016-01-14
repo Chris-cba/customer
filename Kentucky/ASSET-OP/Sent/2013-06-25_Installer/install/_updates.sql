d8144d9204842aa0be660dc336d62ac46b7e5211
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:28:06 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1368 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-OP/Sent/2013-06-25_Installer/install/_updates.sql
@@ -0,0 +1,57 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_types.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_types.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_type_attribs_all.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_type_attribs_all.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_type_x.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_type_x.sql
+SET FEEDBACK OFF
+--
+
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\NM_MRG_query.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\NM_MRG_query.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_OPBD_LOOKUP.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_OPBD_LOOKUP.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\DerivedAsset.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\DerivedAsset.sql
+SET FEEDBACK OFF
+--
+--
+
