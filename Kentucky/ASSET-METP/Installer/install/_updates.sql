cc8adcdf13a46b70c03ce7a59784785c4a85fbf5
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 15:59:44 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1259 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-METP/Installer/install/_updates.sql
@@ -0,0 +1,95 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METP.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METP.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_META_Insert.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_META_Insert.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_META_Insert.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_META_Insert.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_METP_fk.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_METP_fk.sql
+SET FEEDBACK OFF
+--
+--
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\table_x_xnminvitemsall_metc.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\table_x_xnminvitemsall_metc.sql
+SET FEEDBACK OFF
+--
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_xnminvitemsall_metc.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_xnminvitemsall_metc.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\MetC_compound.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\MetC_compound.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\MetP_compound.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\MetP_compound.sql
+SET FEEDBACK OFF
+--
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METP_CSV_LOAD.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METP_CSV_LOAD.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METC_CSV_LOAD.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METC_CSV_LOAD.sql
+SET FEEDBACK OFF
+--
+--
