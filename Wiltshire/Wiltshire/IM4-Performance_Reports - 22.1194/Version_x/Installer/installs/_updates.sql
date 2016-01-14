0b771788de753826f4d63c65e1cacae9cefd5168
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 19:31:30 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@2038 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Wiltshire/Wiltshire/IM4-Performance_Reports - 22.1194/Version_x/Installer/installs/_updates.sql
@@ -0,0 +1,47 @@
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\other\HIG_MODULES.SQL ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\other\HIG_MODULES.SQL
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\other\hig_module_roles.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\other\hig_module_roles.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\other\im_pods.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\other\im_pods.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\other\im_pod_sql.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\other\im_pod_sql.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\other\im_pod_chart.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\other\im_pod_chart.sql
+SET FEEDBACK OFF
+--
+--
