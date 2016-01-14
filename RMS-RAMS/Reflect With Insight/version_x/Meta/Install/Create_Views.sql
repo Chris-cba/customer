2cb542c083af3913b9c8e585667dadc929aefc5a
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 18:58:22 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1895 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ RMS-RAMS/Reflect With Insight/version_x/Meta/Install/Create_Views.sql
@@ -0,0 +1,13 @@
+exec nm3inv.create_view ('RSD', false);
+exec nm3inv.create_view ('RSAM', false);
+exec nm3inv.create_view ('RSDE', false);
+exec nm3inv.create_view ('RSIC', false);
+exec nm3inv.create_view ('RSIN', false);
+exec nm3inv.create_view ('RSRE', false);
+
+exec nm3inv.create_view ('RSD', true);
+exec nm3inv.create_view ('RSAM', true);
+exec nm3inv.create_view ('RSDE', true);
+exec nm3inv.create_view ('RSIC', true);
+exec nm3inv.create_view ('RSIN', true);
+exec nm3inv.create_view ('RSRE', true);
