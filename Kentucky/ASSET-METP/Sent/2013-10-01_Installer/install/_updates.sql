0b3787d625956a2c5725228f9ccd3c963c842c84
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:10:04 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1316 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-METP/Sent/2013-10-01_Installer/install/_updates.sql
@@ -0,0 +1,91 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METP.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METP.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_META_Insert.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_META_Insert.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_META_Insert.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_META_Insert.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xKYCT_METP_fk.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xKYCT_METP_fk.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METP_TRIG.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METP_TRIG.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METP_TRIG_insert.SQL ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METP_TRIG_insert.SQL
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METC_TRIG.SQL ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METC_TRIG.SQL
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METC_TRIG_insert.SQL ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METC_TRIG_insert.SQL
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METP_CSV_LOAD.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METP_CSV_LOAD.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\METC_CSV_LOAD.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\METC_CSV_LOAD.sql
+SET FEEDBACK OFF
+--
+--
