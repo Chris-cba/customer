7a7fc8a7dca3617bf07396732f74abc29b5de3a3
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:22:52 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1352 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-OP/Installer/install/uninstall_derived_asset_type_op.sql
@@ -0,0 +1,78 @@
+
+delete from nm_members_all where nm_obj_type = 'OPBD';
+commit;
+delete from nm_inv_items_all where iit_inv_type = 'OPBD';
+commit;
+delete from NM_MRG_NIT_DERIVATION_NW where nmndn_nmnd_nit_inv_type = 'OPBD';
+commit;
+delete from NM_MRG_ITA_DERIVATION where nmid_ita_inv_type = 'OPBD';
+commit;
+delete from NM_MRG_NIT_DERIVATION where nmnd_nit_inv_type = 'OPBD';
+commit;
+--delete from nm_mrg_query_attribs where nqa_nmq_id = l_merge_id;
+--delete from nm_mrg_query_types where nqt_nmq_id = l_merge_id;
+--delete from nm_mrg_query_roles where nqro_nmq_id = l_merge_id;
+
+declare
+l_merge_id number;
+begin
+    begin
+    select nmq_id into l_merge_id from NM_MRG_QUERY where NMQ_UNIQUE = 'TYPEOP_BD';
+	exception when others then null;
+end;
+
+delete from nm_mrg_query_all where nmq_id = l_merge_id;
+commit;
+end;
+/
+
+delete from nm_inv_type_roles where itr_inv_type = 'OPBD';
+commit;
+delete from nm_inv_type_attribs_all where ita_inv_type = 'OPBD';
+commit;
+delete from nm_inv_nw_all where nin_nit_inv_code = 'OPBD';
+commit;
+delete from nm_inv_types_all where nit_inv_type = 'OPBD';
+
+commit;
+
+
+
+
+
+delete from nm_members_all where nm_obj_type = 'OP';
+commit;
+delete from nm_inv_items_all where iit_inv_type = 'OP';
+commit;
+delete from NM_MRG_NIT_DERIVATION_NW where nmndn_nmnd_nit_inv_type = 'OP';
+commit;
+delete from NM_MRG_ITA_DERIVATION where nmid_ita_inv_type = 'OP';
+commit;
+delete from NM_MRG_NIT_DERIVATION where nmnd_nit_inv_type = 'OP';
+commit;
+--delete from nm_mrg_query_attribs where nqa_nmq_id = l_merge_id;
+--delete from nm_mrg_query_types where nqt_nmq_id = l_merge_id;
+--delete from nm_mrg_query_roles where nqro_nmq_id = l_merge_id;
+
+declare
+l_merge_id number;
+begin
+    begin
+    select nmq_id into l_merge_id from NM_MRG_QUERY where NMQ_UNIQUE = 'TYPEOP';
+	exception when others then null;
+end;
+
+delete from nm_mrg_query_all where nmq_id = l_merge_id;
+commit;
+end;
+/
+
+delete from nm_inv_type_roles where itr_inv_type = 'OP';
+commit;
+delete from nm_inv_type_attribs_all where ita_inv_type = 'OP';
+commit;
+delete from nm_inv_nw_all where nin_nit_inv_code = 'OP';
+commit;
+delete from nm_inv_types_all where nit_inv_type = 'OP';
+commit;
+
