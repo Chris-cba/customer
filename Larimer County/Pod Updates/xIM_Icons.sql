e02d4d8499eabffbdcb8b71886f719fe51274b57
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 18:25:48 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1750 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Larimer County/Pod Updates/xIM_Icons.sql
@@ -0,0 +1,23 @@
+create table xIM_ICONS 
+(
+Imagedir  varchar2(50),
+item  varchar2(50),
+filename  varchar2(50)
+);
+
+REM INSERTING into xIM_ICONS
+SET DEFINE OFF;
+Insert into "xIM_ICONS" (IMAGEDIR,ITEM,FILENAME) values ('http://bip01.assetwiseonline.bentley.com/i/
+','MFCLOSED','mfclosed.gif');
+Insert into "xIM_ICONS" (IMAGEDIR,ITEM,FILENAME) values ('http://bip01.assetwiseonline.bentley.com/i/
+','MFOPEN','mfopen.gif');
+Insert into "xIM_ICONS" (IMAGEDIR,ITEM,FILENAME) values ('http://bip01.assetwiseonline.bentley.com/i/
+','GG','grey_globe.png');
+Insert into "xIM_ICONS" (IMAGEDIR,ITEM,FILENAME) values ('http://bip01.assetwiseonline.bentley.com/i/
+','G64','globe_64.gif');
+Insert into "xIM_ICONS" (IMAGEDIR,ITEM,FILENAME) values ('http://bip01.assetwiseonline.bentley.com/i/
+','SREPORT','sample_report.gif');
+Insert into "xIM_ICONS" (IMAGEDIR,ITEM,FILENAME) values ('http://bip01.assetwiseonline.bentley.com/i/
+','AMBULANCE','ambulance-24x24x8b.png');
+Insert into "xIM_ICONS" (IMAGEDIR,ITEM,FILENAME) values ('http://bip01.assetwiseonline.bentley.com/i/
+','CALC','calculator_go.png');
