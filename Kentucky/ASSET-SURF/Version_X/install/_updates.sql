feb8eda9320388248f82722319cd89465d518d34
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:53:56 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1441 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-SURF/Version_X/install/_updates.sql
@@ -0,0 +1,89 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\NM_INV_ATTRI_LOOKUP_ALL.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\NM_INV_ATTRI_LOOKUP_ALL.sql
+SET FEEDBACK OFF
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_types.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_types.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_type_attribs_all.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_type_attribs_all.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_type_x.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_type_x.sql
+SET FEEDBACK OFF
+--
+
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\NM_MRG_query.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\NM_MRG_query.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_tab_surf_lookup.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_tab_surf_lookup.sql
+SET FEEDBACK OFF
+--
+--SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_surf_LOOKUP.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_surf_LOOKUP.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\DerivedAsset.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\DerivedAsset.sql
+SET FEEDBACK OFF
+--
+--
+----------Admin_Unit_issue
+
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\admin_unit_issue\xky_surf_au.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\admin_unit_issue\xky_surf_au.sql
+SET FEEDBACK OFF
+
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\admin_unit_issue\zz_proceedure_to_update_au.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\admin_unit_issue\zz_proceedure_to_update_au.sql
+SET FEEDBACK OFF
