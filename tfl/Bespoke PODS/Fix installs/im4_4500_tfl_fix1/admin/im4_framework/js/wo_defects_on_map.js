b2e16bf822568bf756db33d26c57716e0ba7afba
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 20:14:24 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@2374 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/Fix installs/im4_4500_tfl_fix1/admin/im4_framework/js/wo_defects_on_map.js
@@ -0,0 +1,46 @@
+/* -----------------------------------------------------------------------------
+ --
+ --   PVCS Identifiers :-
+ --
+ --       pvcsid           : $Header:   //new_vm_latest/archives/customer/tfl/Bespoke PODS/Fix installs/im4_4500_tfl_fix1/admin/im4_framework/js/wo_defects_on_map.js-arc   1.0   Jan 14 2016 20:14:24   Sarah.Williams  $
+ --       Module Name      : $Workfile:   wo_defects_on_map.js  $
+ --       Date into PVCS   : $Date:   Jan 14 2016 20:14:24  $
+ --       Date fetched Out : $Modtime:   Jun 27 2012 17:15:04  $
+ --       PVCS Version     : $Revision:   1.0  $
+ --       Based on SCCS version :
+ --
+ --
+ -----------------------------------------------------------------------------
+ -- Copyright (c) exor corporation ltd, 2009
+ -----------------------------------------------------------------------------
+ */
+function addTheme(pLayerName, pFeatureID){
+   var xthemebasedfoi = new MVThemeBasedFOI(pLayerName, dataSource + '.' + pLayerName);
+   if (pLayerName == 'IM_WORK_ORDER_LINES'){
+    xthemebasedfoi.setQueryParameters( '~', '~', pFeatureID );
+   } else {
+    xthemebasedfoi.setQueryParameters(pFeatureID);
+   } 
+   xthemebasedfoi.setBringToTopOnMouseOver(true);
+   xthemebasedfoi.enableInfoTip(true);
+   xthemebasedfoi.setVisible(true);
+   xthemebasedfoi.setInfoWindowStyle("MVInfoWindowStyle1");
+   xthemebasedfoi.setEventListener("mouse_click", foiClick);
+   xthemebasedfoi.setBoundingTheme(true);
+   mapview.addThemeBasedFOI(xthemebasedfoi);
+}
+
+function showWODefOnMap(pWOID,pWOLID ){
+  
+   $("#mapholder").dialog('open');
+   
+   showMapNoDisplay();
+
+   removeTemplated();
+       
+   addTheme('IM_WORK_ORDER_LINES',pWOID);
+   addTheme('IM_WO_DEFECTS',pWOID);
+
+   mapview.display();
+   
+}
