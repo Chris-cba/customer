5e90f954e4d7cbdb49f1caac64195176ed7b4d23
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:52:02 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1433 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-SURF/Version_X/admin/sql/admin_unit_issue/xky_surf_au.sql
@@ -0,0 +1,13 @@
+create or replace function xky_surf_au(s_hwy_au varchar2, s_nau_admin_type varchar2 := 'INV') return varchar2 as
+	s_temp varchar2(8) := null;
+	n_count number :=0;
+begin
+	
+	select count(*) into n_count from nm_admin_units where nau_admin_type = s_nau_admin_type and nau_name = (select nau_name from nm_admin_units where nau_admin_unit =s_hwy_au and nau_admin_type = 'HWY');
+	if n_count = 1 then
+		select nau_admin_unit into s_temp from nm_admin_units where nau_admin_type = s_nau_admin_type and nau_name = (select nau_name from nm_admin_units where nau_admin_unit =s_hwy_au and nau_admin_type = 'HWY');
+	end if;
+	
+	return s_temp;
+end;
+/
