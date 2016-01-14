499a8e590e1ee2c751a574cba8e73acc04d54a82
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 22:51:06 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@3962 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/For Fix Breakout/im41020/install/_updates.sql
@@ -0,0 +1,37 @@
+|--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im41021-work_due_to_be_cmp_no_df_child.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im41021-work_due_to_be_cmp_no_df_child.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_lohac_daterange_wodc.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_lohac_daterange_wodc.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_sql.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_sql.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_chart.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_chart.sql
+SET FEEDBACK OFF
+--
+--
