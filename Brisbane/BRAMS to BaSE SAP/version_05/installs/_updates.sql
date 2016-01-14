fe4a47691544cc6e5028170d1f6047c8ca668edd
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 17:56:42 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1555 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Brisbane/BRAMS to BaSE SAP/version_05/installs/_updates.sql
@@ -0,0 +1,56 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\index\date_modified.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\index\date_modified.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\tables\XBCC_GLOBAL_TEMP.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\tables\XBCC_GLOBAL_TEMP.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\x_log_table.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\x_log_table.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\x_log_table.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\x_log_table.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\XBCC_SAP_SYNC.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\XBCC_SAP_SYNC.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\XBCC_SAP_SYNC.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\XBCC_SAP_SYNC.pkb
+SET FEEDBACK OFF
+--
+--
+
