bf08bf8369ae30e395dfe62be6ce58e196c18888
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:19:54 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1345 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-OP/Installer/admin/sql/nm_inv_type_x.sql
@@ -0,0 +1,15 @@
+	
+INSERT INTO NM_INV_TYPE_ROLES values
+('OP', 'INV_ALL', 'NORMAL');
+INSERT INTO NM_INV_TYPE_ROLES values
+('OP', 'INV_READONLY', 'READONLY');
+
+
+Insert into EXOR.NM_INV_NW_ALL
+   (NIN_NW_TYPE, NIN_NIT_INV_CODE, NIN_LOC_MANDATORY, NIN_START_DATE)
+ Values
+   ('BD', 'OP', 'N', trunc(sysdate));
+COMMIT;
+
+exec nm3inv.create_view ('OP', false);
+exec nm3inv.create_view ('OP', true);
