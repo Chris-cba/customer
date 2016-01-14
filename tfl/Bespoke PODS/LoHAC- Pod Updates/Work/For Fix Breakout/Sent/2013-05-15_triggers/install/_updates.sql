8774ea9925e0fbe399bf494891bf3558bb0fa2c1
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 23:04:58 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@4054 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/For Fix Breakout/Sent/2013-05-15_triggers/install/_updates.sql
@@ -0,0 +1,28 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\46_3.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\46_3.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\47_5.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\47_5.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xtfl_wo_sec_u_trg.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xtfl_wo_sec_u_trg.sql
+SET FEEDBACK OFF
+--
+--
