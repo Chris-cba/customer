fc1236caf535a7baae0b6b4c4532b19b59ac3ca0
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 18:58:10 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1893 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ RMS-RAMS/Reflect With Insight/version_x/Meta/Install/Create_NW.sql
@@ -0,0 +1,8 @@
+Insert Into nm_inv_nw_all (NIN_NW_TYPE, NIN_NIT_INV_CODE, NIN_LOC_MANDATORY, NIN_START_DATE) Values ('LCWY', 'RSD', 'N', to_date('01-JAN-1901'));
+Insert Into nm_inv_nw_all (NIN_NW_TYPE, NIN_NIT_INV_CODE, NIN_LOC_MANDATORY, NIN_START_DATE) Values ('LCWY', 'RSAM', 'N', to_date('01-JAN-1901'));
+Insert Into nm_inv_nw_all (NIN_NW_TYPE, NIN_NIT_INV_CODE, NIN_LOC_MANDATORY, NIN_START_DATE) Values ('LCWY', 'RSDE', 'N', to_date('01-JAN-1901'));
+Insert Into nm_inv_nw_all (NIN_NW_TYPE, NIN_NIT_INV_CODE, NIN_LOC_MANDATORY, NIN_START_DATE) Values ('LCWY', 'RSIC', 'N', to_date('01-JAN-1901'));
+Insert Into nm_inv_nw_all (NIN_NW_TYPE, NIN_NIT_INV_CODE, NIN_LOC_MANDATORY, NIN_START_DATE) Values ('LCWY', 'RSIN', 'N', to_date('01-JAN-1901'));
+Insert Into nm_inv_nw_all (NIN_NW_TYPE, NIN_NIT_INV_CODE, NIN_LOC_MANDATORY, NIN_START_DATE) Values ('LCWY', 'RSRE', 'N', to_date('01-JAN-1901'));
+
+commit;
