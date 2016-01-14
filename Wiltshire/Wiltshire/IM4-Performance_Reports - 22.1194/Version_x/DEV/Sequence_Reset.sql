8b3e1192e637de87ebe5b2aa42149bdc473d2aeb
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 19:30:06 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@2031 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Wiltshire/Wiltshire/IM4-Performance_Reports - 22.1194/Version_x/DEV/Sequence_Reset.sql
@@ -0,0 +1,14 @@
+drop sequence ip_id_seq;
+
+create sequence ip_id_seq 
+minvalue 1 start with 4000 
+increment by 1 
+nocache;
+
+
+drop sequence IPS_ID_SEQ;
+
+create sequence IPS_ID_SEQ 
+minvalue 1 start with 4000 
+increment by 1 
+nocache;
