4f19389e193043b1e31646e265167d8c805680e2
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 22:50:06 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@3955 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/For Fix Breakout/im41015/install/_updates.sql
@@ -0,0 +1,54 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im41015-c_pod_eop_requests.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im41015-c_pod_eop_requests.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im41015-c_pod_eop_requests_nobud.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im41015-c_pod_eop_requests_nobud.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im41015-c_pod_eot_requests.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im41015-c_pod_eot_requests.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pods.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pods.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_sql.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_sql.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_chart.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_chart.sql
+SET FEEDBACK OFF
+--
