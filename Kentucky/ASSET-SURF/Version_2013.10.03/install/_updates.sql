0d835c8ef05e4f108b71b431f51435d0ba8a54b8
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:35:02 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1385 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-SURF/Version_2013.10.03/install/_updates.sql
@@ -0,0 +1,73 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\NM_INV_ATTRI_LOOKUP_ALL.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\NM_INV_ATTRI_LOOKUP_ALL.sql
+SET FEEDBACK OFF
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_types.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_types.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_type_attribs_all.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_type_attribs_all.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\nm_inv_type_x.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\nm_inv_type_x.sql
+SET FEEDBACK OFF
+--
+
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\NM_MRG_query.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\NM_MRG_query.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_tab_surf_lookup.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_tab_surf_lookup.sql
+SET FEEDBACK OFF
+--
+--SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_surf_LOOKUP.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_surf_LOOKUP.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\DerivedAsset.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\DerivedAsset.sql
+SET FEEDBACK OFF
+--
+--
+
