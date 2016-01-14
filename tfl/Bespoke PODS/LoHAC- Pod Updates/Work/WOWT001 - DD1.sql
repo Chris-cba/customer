c2588f86d8974208be9def4a107f41857e5c2f4e
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 22:32:50 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@3818 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/WOWT001 - DD1.sql
@@ -0,0 +1,45 @@
+select distinct(works_order_number) wor_number
+         ,'<p title="Click for forms"  id="'||works_order_number||'"  onmouseover="showWOLDetails(this);">'||  WORKs_ORDER_NUMBER||'</p>' WORKs_ORDER_NUMBER
+,WORKS_ORDER_DESCRiption
+--, '<a title="Click to view Lines" href="javascript:showDrillDown(&APP_ID.,&SESSION.,30,''P30_WO_ID'','''||WORKs_ORDER_NUMBER||''');">'||number_of_lines||'</a>' 
+,number_of_lines
+,order_estimated_cost
+,date_raised
+,WOR_CHAR_ATTRIB118 "Task Order Status"
+, decode(works_order_has_shape,'N',
+              '<img width=24 height=24 src="/im4_framework/images/grey_globe.png" title="No Location">'
+             ,'<a href="javascript:showWODefOnMap('''||WORKs_ORDER_NUMBER||''',''~'');" ><img width=24 height=24 src="/im4_framework/images/globe_64.gif" title="Find on Map"></a>') map
+,decode(x_im_wo_has_doc(works_order_number,'WORK_ORDERS'),0,
+              '<img width=24 height=24 src="/im4_framework/images/mfclosed.gif" title="No Documents">'
+,'<a href="javascript:showWODocAssocs('''||works_order_number||''',&APP_ID.,&APP_SESSION.,''WORK_ORDERS'')" ><img width=24 height=24 src="/im4_framework/images/mfopen.gif" title="Show Documents"></a>') DOCS
+,APEX_ITEM.CHECKBOX(101,
+                    WORKS_ORDER_NUMBER,
+                    'class="selectme" title="Click to Instruct WO" onclick="openConfirmBox(''Instruct Work Order?'');"',                    
+                    '1',
+                    ':',
+                    'f101_' || ROWNUM
+                   )  "Instruct" 
+,APEX_ITEM.CHECKBOX(102,
+                    works_order_number,
+                      'class="selectme" title="Click to Hold/Reject WO" onclick="openHoldRejectDialog();"',
+                    '1',
+                    ':',
+                    'f102_' || ROWNUM
+                   )  "Hold/Reject" 
+,APEX_ITEM.CHECKBOX(103,
+                    WORKS_ORDER_NUMBER,
+                    'class="selectme" title="Click to Forward WO" onclick="openForwardToDialog();"',
+                    '1',
+                    ':',
+                    'f103_' || ROWNUM
+                   )  "Forward" 
+,APEX_ITEM.CHECKBOX(103,
+                    WORKS_ORDER_NUMBER,
+                    'title="Click to Authorise WO" class="authchk" onclick="openAuthoriseDialog();"' || decode( authorised_by_id,null,null,'disabled="true" checked'),
+                    authorised_by_id,
+                    ':',
+                    'f103_' || ROWNUM
+                   ) "authorised"   
+from  X_WO_TFL_WORK_TRAY_WOW001
+where nvl(defect_priority,'NON') = :P10_PRIORITY
+and    range_value = :P10_DAYS
