bd475c013d6c9ba76afe9f453f238bed8e34a81d
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 23:06:42 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@4069 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/For Fix Breakout/Sent/2013-05-15_wowt001_003/install/_updates.sql
@@ -0,0 +1,109 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_lohac_nobudget_security.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_lohac_nobudget_security.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\ximf_mai_wo_all_attr_no_sec.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\ximf_mai_wo_all_attr_no_sec.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_lohac_daterange_wowt.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_lohac_daterange_wowt.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_lohac_daterange_wowt003.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_lohac_daterange_wowt003.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\wowt003-x_wo_tfl_work_tray_im511003_full.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\wowt003-x_wo_tfl_work_tray_im511003_full.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\wowt001-x_wo_tfl_work_tray_im511001.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\wowt001-x_wo_tfl_work_tray_im511001.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\wowt001-x_wo_tfl_work_tray_im511001_nobud.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\wowt001-x_wo_tfl_work_tray_im511001_nobud.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\wowt003-x_wo_tfl_work_tray_im511003_all.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\wowt003-x_wo_tfl_work_tray_im511003_all.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\wowt003-x_wo_tfl_work_tray_im511003_all_nobud.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\wowt003-x_wo_tfl_work_tray_im511003_all_nobud.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_chart.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_chart.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_sql.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_sql.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\index.ind ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\index.ind
+SET FEEDBACK OFF
+--
+--
