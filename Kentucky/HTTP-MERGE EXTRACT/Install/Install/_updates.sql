79fe9dbe4ed8967da798b7bfdc186684bf618ee4
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:58:52 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1455 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/HTTP-MERGE EXTRACT/Install/Install/_updates.sql
@@ -0,0 +1,28 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\xor_merge_extract.pks ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\xor_merge_extract.pks
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\XOR_MERGE_EXTRACT.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\XOR_MERGE_EXTRACT.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\merge_extract_data1.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\merge_extract_data1.sql
+SET FEEDBACK OFF
+--
+--
