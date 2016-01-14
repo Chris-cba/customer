1454f49fa3d567d8b6e41ba298061932d691d4e0
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 16:27:52 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1367 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Kentucky/ASSET-OP/Sent/2013-06-25_Installer/install/uninstall_derived_asset_type_op.sql
@@ -0,0 +1,26 @@
+
+declare
+l_merge_id number;
+begin
+    begin
+    select nmq_id into l_merge_id from NM_MRG_QUERY where NMQ_UNIQUE = 'TYPEOP_BD';
+	exception when others then null;
+end;
+
+delete from nm_members where nm_obj_type = 'OPBD';
+delete from nm_inv_items where iit_inv_type = 'OPBD';
+delete from NM_MRG_NIT_DERIVATION_NW where nmndn_nmnd_nit_inv_type = 'OPBD';
+delete from NM_MRG_ITA_DERIVATION where nmid_ita_inv_type = 'OPBD';
+delete from NM_MRG_NIT_DERIVATION where nmnd_nit_inv_type = 'OPBD';
+--delete from nm_mrg_query_attribs where nqa_nmq_id = l_merge_id;
+--delete from nm_mrg_query_types where nqt_nmq_id = l_merge_id;
+--delete from nm_mrg_query_roles where nqro_nmq_id = l_merge_id;
+delete from nm_mrg_query where nmq_id = l_merge_id;
+delete from nm_inv_type_roles where itr_inv_type = 'OPBD';
+delete from nm_inv_type_attribs where ita_inv_type = 'OPBD';
+delete from nm_inv_nw_all where nin_nit_inv_code = 'OPBD';
+delete from nm_inv_types where nit_inv_type = 'OPBD';
+
+commit;
+end;
+/
