79927dab12c84cf08b0a27ca195b4d30febe2bf6
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 22:48:28 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@3944 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/For Fix Breakout/ar_as/install/_updates.sql
@@ -0,0 +1,55 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\x_lohac_daterange_wk.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\x_lohac_daterange_wk.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\applicationreview.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\applicationreview.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\applicationstatus.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\applicationstatus.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_sql.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_sql.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\im_pod_chart.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\im_pod_chart.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\f512.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\f512.sql
+SET FEEDBACK OFF
+--
+--
