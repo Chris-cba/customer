b60bf3cfbefeefa321bf7b143747186fdefb3e6c
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-14 18:58:46 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@1900 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ RMS-RAMS/Reflect With Insight/version_x/Meta/Install/_updates.sql
@@ -0,0 +1,69 @@
+SET TERM ON
+PROMPT *** RUNNING: Create_Domain.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START Create_Domain.sql
+SET FEEDBACK OFF
+
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: Create_Assets.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START Create_Assets.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: Create_Attribs.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START Create_Attribs.sql
+SET FEEDBACK OFF
+--
+
+--
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: Create_NW.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START Create_NW.sql
+SET FEEDBACK OFF
+--
+
+--
+
+SET TERM ON
+PROMPT *** RUNNING: Create_Groupings.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START Create_Groupings.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: Create_Roles.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START Create_Roles.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: Create_Views.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START Create_Views.sql
+SET FEEDBACK OFF
+--
+--
