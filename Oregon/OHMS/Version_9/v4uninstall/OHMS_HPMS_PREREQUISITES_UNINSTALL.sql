8efd4451fd44786b59852cca4f7e78249b709213
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 20:42:30 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@5568 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/OHMS/Version_9/v4uninstall/OHMS_HPMS_PREREQUISITES_UNINSTALL.sql
@@ -0,0 +1,41 @@
+/* ---------- JM, 2011.07.25,13:54
+OHMS, HPMS INSERT Script
+---------- */
+
+set sqlblanklines on
+
+
+-- HPMS_CATALOG
+
+	DELETE FROM HPMS_CATALOG WHERE hc_name = 'OHMS';
+
+-- HPMS_TABLE
+
+	DELETE FROM HPMS_TABLE where ht_name = 'OHMS_SUBMIT_SECTIONS';
+
+
+-- HPMS_Column
+	DELETE FROM HPMS_COLUMN where HCL_ID between 500 and 515;
+	
+	
+
+
+
+-- HPMS_MAPPING
+		
+	DELETE FROM HPMS_MAPPING where HM_HCL_ID between 500 and 515;
+		
+
+			
+			
+-- HPMS_PROCEDURE
+	
+	DELETE FROM HPMS_PROCEDURE where hp_db_table_name = 'OHMS_SUBMIT_SECTIONS';
+	
+
+		
+commit;
+
+ALTER TABLE HPMS_COLUMN	Drop Column HCL_SEQ_NAME;
+
+@ zDrop_HPMS_TV.sql;
