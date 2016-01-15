17eb492317ad17d97854f6507c5734be6bc927af
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 20:58:52 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@5663 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/OHMS/Version_X/~notes/Old_installer/OHMS_INSTALL.sql
@@ -0,0 +1,32 @@
+-- install OHMS
+
+-- This application requires HPMS to be installed first.
+
+set sqlblanklines on
+
+@ OHMS_TABLES.sql
+
+--  HPMS Prereqs
+@OHMS_OHMS_MV_CREATE.sql
+@OHMS_OHMS_7_Views.sql
+@OHMS_OHMS_7_Final_views.sql
+
+
+
+-- OHMS
+@ OHMS_SECURITY.sql
+
+@ ohms_menus.sql
+
+--@V_NM_SECW.vw
+
+@ Create_OHMS_merge.sql
+
+@OHMS_SIGNAL_CNT.prc
+@XODOT_POPULATE_OHMS_SECTIONS.prc
+
+@ xodot_OHMS.pks
+
+@ xodot_OHMS.pkb
+
+/
