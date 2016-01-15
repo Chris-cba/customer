3258e09cab8fdfe1d555346d9da94b2a65e4ac02
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 20:18:08 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@5426 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/OHMS/Version_6.0/OHMS_UNINSTALL.sql
@@ -0,0 +1,42 @@
+/*<TOAD_FILE_CHUNK>*/
+
+delete from HIG_STANDARD_FAVOURITES where HSTF_PARENT = 'OHMS';
+
+delete from HIG_STANDARD_FAVOURITES where HSTF_CHILD = 'OHMS';
+
+delete from GRI_MODULES where GRM_MODULE = 'OHMS_RUN';
+
+
+delete from HIG_MODULES where 
+ HMO_APPLICATION = 'OHMS';
+
+declare
+
+l_merge_id NM_MRG_QUERY_ALL.nmq_id%type;
+
+begin
+
+
+    select nmq_id into l_merge_id from NM_MRG_QUERY_ALL where NMQ_UNIQUE = 'OHMS_EXTRACT';
+    
+    delete from  NM_MRG_QUERY_ALL where NMQ_ID = l_merge_id;
+    
+    exception
+    when others then null;
+    
+end;
+/
+/*<TOAD_FILE_CHUNK>*/
+
+DROP TABLE V_NM_OHMS_NW CASCADE CONSTRAINTS;
+DROP TABLE OHMS_SUBMIT_SAMPLES;
+DROP TABLE OHMS_ACTIVITY_LOG;
+
+
+@zDrop_OHMS_TV.sql
+
+
+drop Procedure XODOT_POPULATE_OHMS_SECTIONS;  --just using the item straight form HPMS
+
+drop package xodot_OHMS;
+
