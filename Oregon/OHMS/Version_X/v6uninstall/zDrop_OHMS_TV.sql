0370c439397e048905ca280c7198c33cfac8ec9d
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 20:50:42 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@5615 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/OHMS/Version_X/v6uninstall/zDrop_OHMS_TV.sql
@@ -0,0 +1,29 @@
+set echo on
+
+declare
+    i integer;
+    sname varchar(50);
+
+
+BEGIN  --  Done to remove error messages
+	
+	
+	sname := 'OHMS_SUBMIT_SAMPLES';
+
+	
+    select COUNT(*) into I from USER_TABLES where TABLE_NAME = SNAME;
+    if i <> 0 then
+		-- Dropping 'OHMS_SUBMIT_SAMPLES'
+        execute immediate 'drop table ' || sname || ' CASCADE CONSTRAINTS' ;
+    end if;
+	
+	sname := 'V_NM_OHMS_NW';	
+    select COUNT(*) into I from USER_TABLES where TABLE_NAME = SNAME;
+    if i <> 0 then
+		-- Dropping 'TRANSINFO.V_NM_OHMS_NW'
+        execute immediate 'drop table ' || sname || ' CASCADE CONSTRAINTS' ;
+    end if;
+	
+END;
+
+/
