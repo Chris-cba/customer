7cfa61fc0c34ac75ae09db59d913a8e6747df3aa
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 18:51:32 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@5170 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/gasb34/Version_5/Version_5/uninstall_gasb34_triggers_sql.sql
@@ -0,0 +1,12 @@
+-- script to install GASB34 triggers.
+
+drop  package TRANSINFO.xodot_elements_state;
+
+
+drop  TRIGGER TRANSINFO.xodot_ins_b_elements;
+
+drop TRIGGER xodot_upd_trnf_grp_mem_attribs;
+
+
+drop TRIGGER TRANSINFO.xodot_upd_a_elements_trans;
+
