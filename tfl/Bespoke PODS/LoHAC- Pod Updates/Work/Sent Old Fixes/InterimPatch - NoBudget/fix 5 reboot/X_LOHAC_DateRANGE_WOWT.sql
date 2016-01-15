6db9c17b7773351d82124deab8077dfeb6bf2807
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 00:40:12 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@4994 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/Sent Old Fixes/InterimPatch - NoBudget/fix 5 reboot/X_LOHAC_DateRANGE_WOWT.sql
@@ -0,0 +1,21 @@
+Create or replace view X_LOHAC_DateRANGE_WOWT as
+		SELECT 1 sorter,
+		SYSDATE - 1				st_range,
+		SYSDATE - 0 - (1/86400) end_range,
+		'0-1' 					range_value
+	FROM DUAL
+	UNION
+		SELECT 2 sorter,
+		SYSDATE - 7				st_range,
+		SYSDATE  - 1 - (1/86400)	end_range,
+		'2-7' 					range_value
+	FROM DUAL
+    UNION
+        SELECT 3 sorter,        
+        SYSDATE  - 360 - (1/86400)    st_range,
+        SYSDATE - 7                end_range,
+        '>7'                     range_value
+        from dual
+        ;
+
+
