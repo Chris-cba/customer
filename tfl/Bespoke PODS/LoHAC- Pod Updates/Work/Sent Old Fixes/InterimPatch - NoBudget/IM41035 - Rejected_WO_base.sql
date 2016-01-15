d978d4b5e16c917ef7b0e786b0355b52ad346c0a
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 00:37:58 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@4970 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ tfl/Bespoke PODS/LoHAC- Pod Updates/Work/Sent Old Fixes/InterimPatch - NoBudget/IM41035 - Rejected_WO_base.sql
@@ -0,0 +1,34 @@
+ 
+ create or replace view X_LOHAC_IM_IM41035_POD as
+ With Haud as 
+        (SELECT HAUD_PK_ID HAUD_PK_ID, HAUD_NEW_VALUE,
+                        HAUD_ATTRIBUTE_NAME HAUD_ATTRIBUTE_NAME,
+                        MAX (HAUD_TIMESTAMP) HAUD_TIMESTAMP, range_value
+                   FROM HIG_AUDITS, POD_DAY_RANGE r
+                  WHERE 1=1
+                        AND HAUD_TABLE_NAME = 'WORK_ORDERS'
+                        and HAUD_ATTRIBUTE_NAME = 'WOR_CHAR_ATTRIB100'
+                        AND HAUD_NEW_VALUE = 'REJ'
+                        AND HAUD_TIMESTAMP between     r.st_range AND r.end_range                
+               GROUP BY HAUD_PK_ID, HAUD_NEW_VALUE, HAUD_ATTRIBUTE_NAME, range_value) 
+           --
+SELECT  
+    range_value days,
+    1 reason,
+     wor_works_order_no works_order_number,
+    WOR_CHAR_ATTRIB104,
+     WOR_CHAR_ATTRIB100
+FROM work_orders wor,
+    work_order_lines wol,
+    haud,    
+    pod_nm_element_security,
+    pod_budget_security
+WHERE    1=1
+    AND wor_works_order_no = haud_pk_id    
+    AND wor_works_order_no =  wol_works_order_no
+    AND wor.WOR_CHAR_ATTRIB100 = HAUD_NEW_VALUE  
+    AND upper(NVL (wor_DESCR, 'Empty')) NOT LIKE '%**CANCELLED**%'
+    AND WOL_STATUS_CODE NOT IN  ('COMPLETED', 'ACTIONED', 'INSTRUCTED', 'PRELOHAC' )
+    AND pod_nm_element_security.element_id = wol_rse_he_id
+    AND pod_budget_security.budget_code = WOL_ICB_WORK_CODE
+    ;
