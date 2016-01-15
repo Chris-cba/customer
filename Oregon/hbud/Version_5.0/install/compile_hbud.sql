f9ebec058b444117c1f14e2cc9dea72876b424e2
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-15 19:27:26 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@5201 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/hbud/Version_5.0/install/compile_hbud.sql
@@ -0,0 +1,29 @@
+--
+-----------------------------------------------------------------------------
+--
+--   PVCS Identifiers :-
+--
+--       pvcsid                 : $Header:   //new_vm_latest/archives/customer/Oregon/hbud/Version_5.0/install/compile_hbud.sql-arc   1.0   Jan 15 2016 19:27:26   Sarah.Williams  $
+--       Module Name      : $Workfile:   compile_hbud.sql  $
+--       Date into PVCS   : $Date:   Jan 15 2016 19:27:26  $
+--       Date fetched Out : $Modtime:   Sep 09 2010 20:39:46  $
+--       PVCS Version     : $Revision:   1.0  $
+--
+--
+--   Author : P Stanton
+--
+-----------------------------------------------------------------------------
+--	Copyright (c) exor corporation ltd, 2010
+-----------------------------------------------------------------------------
+
+DECLARE
+  v_view_name VARCHAR2(30);
+BEGIN
+
+IF NOT nm3ddl.does_object_exist(nm3inv_view.work_out_inv_type_view_name ('HACT'),'VIEW') THEN
+  nm3inv.Create_inv_view('HACT', FALSE, v_view_name);
+END IF;
+
+END;
+/
+alter package XODOT_HBUD_EXTRACT_PROCESS compile;
