bd3bd8dbfb17a259986da3113114401e25b1ef9d
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 22:45:06 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@6018 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/Signs_TI_API/Version_01_14/admin/sql/other/api_custom_types.sql
@@ -0,0 +1,59 @@
+/*
+	The contents of this document, including system ideas and concepts, 
+	are confidential and proprietary in nature and are not to be distributed 
+	in any form without the prior written consent of Bentley Systems.
+	
+	file: api_custom_types.sql
+	Author: JMM
+	UPDATE01:	Original, 2015.03.12, JMM
+*/
+
+
+
+/* ************************************************************
+	Object:		xodot_signs_task_list_row, xodot_signs_task_list, xodot_signs_asset_op
+	Purpose:	Used as interface items between the Signs Broker and the xodot_signs_api
+	Notes:		
+	Created:	2015.06.05	J.Mendoza
+************************************************************* */
+
+/*
+	Used for: Retrieving a task list count and passing it to the broker
+*/
+create or replace type xodot_signs_task_list_row as object (asset varchar2(4), cnt number);
+create or replace type xodot_signs_task_list is table of xodot_signs_task_list_row;
+
+/*
+	Used for: passing insertion status back to the broker
+*/
+create or replace type xodot_signs_asset_op as object (ne_id number(10), err_num number, err_msg varchar2(500));
+
+------------------------SYNONYM------------------------
+------------------------SYNONYM------------------------
+------------------------SYNONYM------------------------
+
+DECLARE
+   name_array dbms_sql.varchar2_table;
+   i_count  number;
+BEGIN
+   name_array(1) := 'xodot_signs_task_list_row';
+   name_array(2) := 'xodot_signs_task_list';
+   name_array(3) := 'xodot_signs_asset_op';
+   --
+   FOR i IN name_array.FIRST .. name_array.LAST
+   LOOP
+	select count(*) into i_count from dba_objects where object_name = upper(name_array(i)) and object_type like 'SYNONYM';
+		if i_count = 1 then
+			execute immediate 'drop PUBLIC SYNONYM ' || name_array(i);
+		end if;
+	select count(*) into i_count from user_objects where object_name = upper(name_array(i)) and object_type = 'SYNONYM';
+		if i_count = 1 then
+			execute immediate 'drop SYNONYM ' || name_array(i);	
+		end if;
+   END LOOP;
+END;
+/
+
+CREATE OR REPLACE PUBLIC SYNONYM xodot_signs_task_list_row FOR TRANSINFO.xodot_signs_task_list_row;
+CREATE OR REPLACE PUBLIC SYNONYM xodot_signs_task_list FOR TRANSINFO.xodot_signs_task_list;
+CREATE OR REPLACE PUBLIC SYNONYM xodot_signs_asset_op FOR TRANSINFO.xodot_signs_asset_op;
