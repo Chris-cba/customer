8e8fa102276be5d066519b65b1733dc2b241ca92
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 22:16:12 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@5852 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/Signs_TI_API/Version_01_01/admin/sql/packages/xodot_signs_logging_tables_seq.sql
@@ -0,0 +1,33 @@
+--drop table xsign_except;
+--drop table reports.xsign_status;
+
+
+Create table xsign_except (
+	SYNC_DATE			date
+	,session_id			number
+	,TI_USERNAME		varchar2(20)
+	,OS_HOST			varchar2(32)
+	,OS_USERNAME		varchar2(20)
+	,NE_ID				varchar2(10)
+	,INSTALLATION_ID	varchar2(10)
+	,STATEHWY			varchar2(10)
+	,MP					number
+	,ERROR_TYPE			varchar2(20)
+	,SYNC_ERROR			varchar2(250)
+);
+
+
+Create table REPORTS.XSIGN_STATUS  (
+	SYNC_DATE			date
+	,TI_USERNAME		varchar2(20)
+	,OS_HOST			varchar2(32)
+	,OS_USERNAME		varchar2(20)
+	,SFA_CNT			number
+	,SFA_EXP			number
+	,TI_CNT				number
+	,TI_EXCP			number
+	,LOV_CNT			number
+	,LOV_EXP			number
+	,MP100_CNT			number
+	,MP100_EXP			number
+);
