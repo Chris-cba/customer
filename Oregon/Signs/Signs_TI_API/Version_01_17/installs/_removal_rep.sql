656aa73c6c901966960c23ea1b181700b6891654
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 22:55:42 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@6090 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/Signs_TI_API/Version_01_17/installs/_removal_rep.sql
@@ -0,0 +1,55 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: drop package body reports.xodot_rollup_mp; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop package body reports.xodot_rollup_mp;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: drop package reports.xodot_rollup_mp; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop package reports.xodot_rollup_mp;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: drop table REPORTS.X_SIGNS_TBLHWYLKUP_IN; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop table REPORTS.X_SIGNS_TBLHWYLKUP_IN;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: drop table REPORTS.X_SIGNS_TBLHWYLKUP_OUT; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop table REPORTS.X_SIGNS_TBLHWYLKUP_OUT;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: drop table reports.xsign_except; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop table reports.xsign_except;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: drop table REPORTS.XSIGN_STATUS; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop table REPORTS.XSIGN_STATUS;
+SET FEEDBACK OFF
+--
+--
