02424e1c89d536075dd8278bf740d989237b913a
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 22:55:58 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@6092 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/Signs_TI_API/Version_01_17/installs/_updates_rep.sql
@@ -0,0 +1,37 @@
+
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_signs_logging_tables_seq.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_signs_logging_tables_seq.sql
+SET FEEDBACK OFF
+
+
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\report_lookup_tables.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\report_lookup_tables.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_rollup_mp.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_rollup_mp.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_rollup_mp.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_rollup_mp.pkb
+SET FEEDBACK OFF
