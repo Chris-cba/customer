ae5441cf60b030915df509c5eaf2479fe0de4dce
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 22:49:34 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@6049 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/Signs_TI_API/Version_01_15/installs/_removal_ti.sql
@@ -0,0 +1,81 @@
+--
+SET TERM ON
+PROMPT *** RUNNING:drop type xodot_signs_task_list; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop type xodot_signs_task_list;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop type xodot_signs_task_list_row; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop type xodot_signs_task_list_row;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop type xodot_signs_asset_op; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop type xodot_signs_asset_op;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop index xsigns_date_chk_idx; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop index xsigns_date_chk_idx;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop trigger xodot_signs_asset_domain_trg; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop trigger xodot_signs_asset_domain_trg;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop package body xodot_signs_api; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop package body xodot_signs_api;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop package  xodot_signs_api; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop package  xodot_signs_api;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop package body xodot_signs_logging; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop package body xodot_signs_logging;
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING:drop package xodot_signs_logging; ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+drop package xodot_signs_logging;
+SET FEEDBACK OFF
+
