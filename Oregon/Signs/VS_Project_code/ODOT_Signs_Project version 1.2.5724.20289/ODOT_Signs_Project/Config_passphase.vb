0fab173d4fa51be91e68aded1de5eea7b7e76fe4
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 23:01:04 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@6127 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/VS_Project_code/ODOT_Signs_Project version 1.2.5724.20289/ODOT_Signs_Project/Config_passphase.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Windows.Forms
+
+Public Class dia_passphase
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+
+        main_form.g_passphrase = TextBox1.Text
+
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub dia_passphase_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Enter Pass Phrase.  Press Cancel to Resume Normal Operation"
+        TextBox1.UseSystemPasswordChar = True
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.CheckState = CheckState.Checked Then
+            TextBox1.UseSystemPasswordChar = False
+        Else
+            TextBox1.UseSystemPasswordChar = True
+        End If
+    End Sub
+End Class
