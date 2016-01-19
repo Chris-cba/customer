a321d290129b6a9e5c16238dde3303e9ec294475
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 23:01:48 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@6133 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/VS_Project_code/ODOT_Signs_Project version 1.2.5724.20289/ODOT_Signs_Project/Fatal_error.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Windows.Forms
+
+Public Class dialog_abort_program
+    Public g_s_issue As String
+    Public g_s_title As String = "Fatal Error"
+    Public g_s_error As String
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub dialog_abort_program_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = g_s_title
+        TB_issue.Text = g_s_issue
+        TB_Error.Text = g_s_error
+    End Sub
+End Class
