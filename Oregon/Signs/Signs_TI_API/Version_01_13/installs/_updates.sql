19d023940e371bf184997b4d59be0ede0ca393db
Sarah.Williams <Sarah.Williams@37afec09-bd54-7940-9fac-cf987e4a02e6>
2016-01-19 22:44:16 +0000

    Initial revision.
    git-svn-id: svn://127.0.0.1/customer/trunk@6017 37afec09-bd54-7940-9fac-cf987e4a02e6

---
+++ Oregon/Signs/Signs_TI_API/Version_01_13/installs/_updates.sql
@@ -0,0 +1,64 @@
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\other\api_custom_types.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\other\api_custom_types.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_signs_logging_tables_seq.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_signs_logging_tables_seq.sql
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_signs_logging.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_signs_logging.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_signs_logging.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_signs_logging.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_signs_api.pkh ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_signs_api.pkh
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\packages\xodot_signs_api.pkb ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\packages\xodot_signs_api.pkb
+SET FEEDBACK OFF
+--
+--
+SET TERM ON
+PROMPT *** RUNNING: ..\admin\sql\triggers\xodot_signs_asset_domain_trg.sql ***
+SET TERM OFF
+--
+SET FEEDBACK ON
+START ..\admin\sql\triggers\xodot_signs_asset_domain_trg.sql
+SET FEEDBACK OFF
+--
+--
